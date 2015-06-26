8e7216bc37b28f2a4466d67b2f64c7c74846e07d
paugier <pierre.augier@ens-lyon.org>
2015-06-26 19:10:30 +0200

    Work on tutorials with Ipython notebooks.

--- doc/conf.py
+++ doc/conf.py
@@ -125,7 +125,7 @@ html_theme = 'sphinxdoc'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'FluidSim '+release
+html_short_title = 'FluidSim ' + release
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
--- doc/tutorials.rst
+++ doc/tutorials.rst
@@ -1,6 +1,8 @@
 Tutorials
 =========
 
+Most of these tutorials have been produced by Ipython notebook.
+
 .. toctree::
    :maxdepth: 2
 
