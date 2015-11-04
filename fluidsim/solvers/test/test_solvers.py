f3c8a7e408dd77abb2d87e14cfe0da367b00d291
paugier <pierre.augier@ens-lyon.org>
2015-11-04 14:32:37 +0100

    Solve silly bug sw1l.

--- fluidsim/solvers/test/test_solvers.py
+++ fluidsim/solvers/test/test_solvers.py
@@ -7,7 +7,7 @@ import fluiddyn.util.mpi as mpi
 from fluiddyn.io import stdout_redirected
 
 
-def run_mini_simul(key_solver):
+def run_mini_simul(key_solver, HAS_TO_SAVE=False):
 
     Simul = fluidsim.import_simul_class_from_key(key_solver)
 
@@ -15,7 +15,7 @@ def run_mini_simul(key_solver):
 
     params.short_name_type_run = 'test'
 
-    nh = 64
+    nh = 32
     params.oper.nx = nh
     params.oper.ny = nh
     Lh = 6.
@@ -31,11 +31,16 @@ def run_mini_simul(key_solver):
     except AttributeError:
         pass
 
+
+
     params.time_stepping.t_end = 0.5
 
     params.init_fields.type = 'dipole'
 
-    params.output.HAS_TO_SAVE = False
+    if HAS_TO_SAVE:
+        params.output.periods_save.spectra = 0.5
+
+    params.output.HAS_TO_SAVE = HAS_TO_SAVE
 
     with stdout_redirected():
         sim = Simul(params)
@@ -53,7 +58,7 @@ class TestSolvers(unittest.TestCase):
 
     def test_sw1l(self):
         """Should be able to run a SW1L simul."""
-        run_mini_simul('SW1L')
+        run_mini_simul('SW1L', HAS_TO_SAVE=True)
 
     def test_sw1l_onlywaves(self):
         """Should be able to run a SW1L.onlywaves simul."""
