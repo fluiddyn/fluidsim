7ea4044f6bb9e1af6be7eeca05148aa7d2629f28
Ashwin Vishnu <avmo@kth.se>
2016-01-07 12:48:26 +0100

    Small bug in path var of scripts/util/resume_from_path.py

--- scripts/util/resume_from_path.py
+++ scripts/util/resume_from_path.py
@@ -2,7 +2,7 @@ import argparse
 import fluidsim as fls
 
 parser = argparse.ArgumentParser(description='Resume a Fluidsim simulation from a path')
-parser.add_argument('PATH',
+parser.add_argument('path',
     help='path to an incomplete simulation directory')
 
 args = parser.parse_args()
