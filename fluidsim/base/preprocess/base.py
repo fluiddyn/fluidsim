b0520c87a679967442f46b4475bd7d9e3a4288de
Ashwin Vishnu <avmo@kth.se>
2016-04-05 17:27:12 +0200

    Continue copying edits from dev: preprocess init_field_const parameter introduced, and apply forcing_time_scheme parameter in onlywaves solver.py

--- fluidsim/base/preprocess/base.py
+++ fluidsim/base/preprocess/base.py
@@ -33,6 +33,7 @@ class PreprocessBase(object):
         """
         attribs = {'enable': False,
                    'init_field_scale': 'energy',
+                   'init_field_const': '1.',
                    'viscosity_type': 'laplacian',
                    'viscosity_scale': 'enstrophy_forcing',
                    'viscosity_const': 5.,
@@ -52,7 +53,6 @@ class PreprocessBase(object):
         self.oper = sim.oper
         self.output = sim.output
 
-        # dict_classes = sim.info.solver.classes.Preprocess.import_classes()
-
     def __call__(self):
-        pass
+        if self.params.enable:
+            self.output.print_stdout('Preprocessing initial fields, and other parameters.')
