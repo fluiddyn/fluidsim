b0063ad385e4bca3ffc58ac95548b29c314a1009
paugier <pierre.augier@ens-lyon.org>
2017-01-25 22:21:06 +0100

    Minimal Lorenz model solver.

--- fluidsim/solvers/models0d/__init__.py
+++ fluidsim/solvers/models0d/__init__.py
@@ -7,5 +7,6 @@ Provides:
    :toctree:
 
    predaprey
+   lorenz
 
 """
---
+++ fluidsim/solvers/models0d/lorenz/output/__init__.py
@@ -0,0 +1,43 @@
+"""Output (:mod:`fluidsim.solvers.models0d.predaprey.output`)
+=============================================================
+
+Provides the modules:
+
+.. autosummary::
+   :toctree:
+
+   print_stdout
+
+and the main output class for this solver:
+
+.. autoclass:: Output
+   :members:
+   :private-members:
+
+"""
+from __future__ import division
+
+from fluidsim.base.output import OutputBase
+
+
+class Output(OutputBase):
+    """Output for Lorenz solver."""
+    @staticmethod
+    def _complete_info_solver(info_solver):
+        """Complete the `info_solver` container (static method)."""
+
+        OutputBase._complete_info_solver(info_solver)
+
+        classes = info_solver.classes.Output.classes
+        package = 'fluidsim.solvers.models0d.lorenz.output'
+
+        classes.PrintStdOut.module_name = package + '.print_stdout'
+        classes.PrintStdOut.class_name = 'PrintStdOutLorenz'
+
+    @staticmethod
+    def _complete_params_with_default(params, info_solver):
+        """Complete the `params` container (static method)."""
+        OutputBase._complete_params_with_default(
+            params, info_solver)
+
+        params.output.phys_fields.field_to_plot = 'X'
