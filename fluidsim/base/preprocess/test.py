4290299bc00799f8e4ba757063202bc4b7fc85b5
Ashwin Vishnu <avmo@kth.se>
2017-07-03 15:56:34 +0200

    Preprocess test
    --HG--
    branch : dev

---
+++ fluidsim/base/preprocess/test.py
@@ -0,0 +1,42 @@
+import unittest
+from shutil import rmtree
+import numpy as np
+
+from fluidsim.base.solvers.pseudo_spect import SimulBasePseudoSpectral
+from fluiddyn.io import stdout_redirected
+
+
+class TestPreprocessPS(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Should be able to run a base experiment."""
+        cls.params = params = SimulBasePseudoSpectral.create_default_params()
+        nh = 16
+        Lh = 2 * np.pi
+        params.oper.nx = nh
+        params.oper.ny = nh
+        params.oper.Lx = Lh
+        params.oper.Ly = Lh
+        params.init_fields.type = 'constant'
+        params.preprocess.enable = True
+        params.preprocess.viscosity_type = 'laplacian'
+        params.preprocess.viscosity_scale = 'energy'
+
+        with stdout_redirected():
+            cls.sim = SimulBasePseudoSpectral(params)
+
+    @classmethod
+    def tearDownClass(cls):
+        rmtree(cls.sim.output.path_run)
+
+    def test_set_viscosity(self):
+        """Tests if preprocess can initialize viscosity or not."""
+        params = self.params
+        sim = self.sim
+        self.assertGreater(sim.params.nu_2, 0)
+        self.assertEqual(sim.params.nu_4 + sim.params.nu_8, 0)
+        assert np.any(sim.time_stepping.freq_lin > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
