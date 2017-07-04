9fa3b9177eed680df39eb87768d59f54a619e429
Ashwin Vishnu <avmo@kth.se>
2017-07-04 20:10:47 +0200

    Try to fix init_fields error in python 3

--- fluidsim/solvers/ns2d/init_fields.py
+++ fluidsim/solvers/ns2d/init_fields.py
@@ -163,6 +163,6 @@ class InitFieldsNS2D(InitFieldsBase):
     @staticmethod
     def _complete_info_solver(info_solver):
         """Complete the `info_solver` container (static method)."""
-        InitFieldsBase._complete_info_solver(
+        super(InitFieldsNS2D, InitFieldsNS2D)._complete_info_solver(
             info_solver, classes=[
                 InitFieldsNoise, InitFieldsJet, InitFieldsDipole])
