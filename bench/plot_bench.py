efe560e2e16f61bd8310e809819b176c7ff9b917
paugier <pierre.augier@ens-lyon.org>
2017-09-26 16:30:00 +0200

    more benchmarks on LEGI cluster

--- bench/plot_bench.py
+++ bench/plot_bench.py
@@ -1,5 +1,4 @@
 
-import os
 from glob import glob
 import json
 
@@ -7,28 +6,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 key_solver = 'ns2d'
+hostname = 'cl7'
 
 dicts = []
 
 for path in glob('results_bench/*'):
     with open(path) as f:
-        dicts.append(json.load(f))
+        d = json.load(f)
+    if not d['hostname'].startswith(hostname):
+        continue
+    dicts.append(d)
+
+
 
 df = pd.DataFrame(dicts)
-df1 = df.loc[df['nb_proc'] == 1]
-t_elapsed1 = df1['t_elapsed'].mean()
-times = df['t_elapsed']
-
-fig = plt.figure()
-ax = plt.subplot()
-ax.plot(df['nb_proc'], t_elapsed1/times, 'or')
-ax.set_title('speed up')
 
 
-fig = plt.figure()
-ax = plt.subplot()
-ax.plot(df['nb_proc'], t_elapsed1/times/df['nb_proc'], 'or')
-ax.set_title('normalized speed up')
+def plot_bench(nb_proc0=1):
+    df0 = df.loc[df['nb_proc'] == nb_proc0]
+    t_elapsed0 = df0['t_elapsed'].mean()
+    times = df['t_elapsed']
+
+    plt.figure()
+    ax = plt.subplot()
+    ax.plot(df['nb_proc'], nb_proc0*t_elapsed0/times, 'xr')
+    tmp = [nb_proc0, df['nb_proc'].max()]
+    ax.plot(tmp, tmp, 'b-')
+    ax.set_title('speed up')
+
+
+plot_bench(nb_proc0=1)
+plot_bench(nb_proc0=2)
 
 
 plt.show()
