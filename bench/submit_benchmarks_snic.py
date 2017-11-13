9d00b02f809d8b74f4b4a28f65b46d9393aa2868
Ashwin Vishnu <avmo@kth.se>
2017-11-13 11:58:01 +0100

    Fix weak scaling scripts because "all" option does not work

--- bench/submit_benchmarks_snic.py
+++ bench/submit_benchmarks_snic.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 import os
-import numpy as np
 from fluiddyn.clusters.snic import ClusterSNIC as Cluster
 import fluidsim
+from fluidsim.util.console import bench
 
 
 # Parameters
 # ----------
-## n0 = 2 ** 10; 'Triolith / Beskow'
+# n0 = 2 ** 10; 'Triolith / Beskow'
 n0 = 1024; nb_cores = [2, 4, 8, 16, 32]; nodes = [2, 4, 8]
-## n0 = 2**6 * 3**2 * 7; 'Kebnekaise'
+# n0 = 2**6 * 3**2 * 7; 'Kebnekaise'
 # n0 = 1008; nb_cores = [2, 4, 8, 12, 16, 21, 24, 28]; nodes = [2, 3, 4, 6]
 
-argv = dict(dim='2d', nh='{} -d 2'.format(n0), time='00:20:00')  # 2D benchmarks
-# argv = dict(dim='3d', nh='960 960 240', time='00:50:00')  # 3D benchmarks
+# 2D benchmarks
+argv = dict(
+    dim='2d', nh='{} {}'.format(n0, n0), time='00:20:00',
+    weak=True,
+    fft=[
+        'fft2d.mpi_with_fftw1d',
+        'fft2d.mpi_with_fftwmpi2d',
+    ]
+
+# 3D benchmarks
+# argv = dict(dim='3d', nh='960 960 240', time='00:50:00')
 
 solver = 'ns2d'
 
@@ -24,16 +33,17 @@ mode = 'inter-intra'
 dry_run = False
 # dry_run = True
 
+
 def init_cluster():
     global output_dir
 
     cluster = Cluster()
     if cluster.name_cluster == 'beskow':
         cluster.default_project = '2016-34-10'
-        interactive=True
+        interactive = True
     else:
         cluster.default_project = 'SNIC2016-34-10'
-        interactive=True
+        interactive = True
 
     output_dir = os.path.abspath('{}/../doc/benchmarks/snic_{}_{}'.format(
         os.path.split(fluidsim.__file__)[0], cluster.name_cluster, argv['dim']))
@@ -45,12 +55,12 @@ def init_cluster():
     return cluster, interactive
 
 
-def submit(cluster, interactive, nb_nodes, nb_cores_per_node=None, shape=None):
+def submit(cluster, interactive, nb_nodes, nb_cores_per_node=None, shape=None, fft='all'):
     if nb_cores_per_node is None:
         nb_cores_per_node = cluster.nb_cores_per_node
 
     nb_mpi = nb_cores_per_node * nb_nodes
-    cmd = 'fluidsim bench -s {} {} -o {}'.format(solver, shape, output_dir)
+    cmd = 'fluidsim bench -s {} {} -t {} -o {}'.format(solver, shape, fft, output_dir)
     if dry_run:
         print('nb_mpi = ', nb_mpi, end=' ')
         print(cmd)
@@ -67,13 +77,34 @@ def submit(cluster, interactive, nb_nodes, nb_cores_per_node=None, shape=None):
 
 if __name__ == '__main__':
     cluster, interactive = init_cluster()
+    if argv['weak']:
+        _shapes = [int(n) for n in argv['nh'].split()]
+        _shapes = bench.estimate_shapes_weak_scaling(
+            *_shapes, nproc_max=nodes[-1] * cluster.nb_cores_per_node)
+
     if 'intra' in mode:
         nb_nodes = 1
-        for nb_cores_per_node in nb_cores:
+        for nb_cores_per_node, type_fft in zip(nb_cores, argv['fft']):
             if nb_cores_per_node > cluster.nb_cores_per_node:
                 continue
-            submit(cluster, interactive, nb_nodes, nb_cores_per_node, shape=argv['nh'])
+            if argv['weak']:
+                try:
+                    shape = _shapes[nb_cores_per_node]
+                except KeyError:
+                    continue
+            else:
+                shape = argv['nh']
+
+            submit(cluster, interactive, nb_nodes, nb_cores_per_node, shape=shape, fft=type_fft)
 
     if 'inter' in mode:
-        for nb_nodes in nodes:
-            submit(cluster, interactive, nb_nodes, shape=argv['nh'])
+        for nb_nodes, type_fft in zip(nodes, argv['fft']):
+            if argv['weak']:
+                try:
+                    shape = _shapes[nb_nodes * cluster.nb_cores_per_node]
+                except KeyError:
+                    continue
+            else:
+                shape = argv['nh']
+
+            submit(cluster, interactive, nb_nodes, shape=shape, fft=type_fft)
