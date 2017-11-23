b9fa4a09eecc8201f3a137bbc482d5fae17e623b
calpe <miguel.calpe-linares@univ-grenoble-alpes.fr>
2017-11-23 15:44:56 +0100

    clean up strat.output.__init__.py

--- fluidsim/solvers/ns2d/strat/output/__init__.py
+++ fluidsim/solvers/ns2d/strat/output/__init__.py
@@ -50,41 +50,24 @@ class OutputStrat(Output):
         classes.PrintStdOut.module_name = base_name_mod + '.print_stdout'
         classes.PrintStdOut.class_name = 'PrintStdOutNS2DStrat'
 
-        classes.PhysFields.class_name = 'PhysFieldsBase2D'
+        classes.PhysFields.module_name = base_name_mod + '.phys_fields'
+        classes.PhysFields.class_name = 'PhysFields2DStrat'
 
         attribs = {
             'module_name': base_name_mod + '.spectra',
             'class_name': 'SpectraNS2DStrat'}
         classes.Spectra._set_attribs(attribs)
 
-        # classes._set_child(
-        #    'Spectra',
-        #    attribs={'module_name': base_name_mod + '.spectra',
-        #             'class_name': 'SpectraNS2DStrat'})
-
         attribs = {
             'module_name': base_name_mod + '.spatial_means',
             'class_name': 'SpatialMeansNS2DStrat'}
         classes.spatial_means._set_attribs(attribs)
 
-        # classes._set_child(
-        #     'spatial_means',
-        #     attribs={'module_name': base_name_mod + '.spatial_means',
-        #              'class_name': 'SpatialMeansNS2DStrat'})
         attribs = {
             'module_name': base_name_mod + '.spect_energy_budget',
             'class_name': 'SpectralEnergyBudgetNS2DStrat'}
         classes.spect_energy_budg._set_attribs(attribs)
 
-        # attribs = {
-        #     'module_name': base_name_mod + '.spect_energy_budget',
-        #     'class_name': 'SpectralEnergyBudgetNS2DStrat'}
-        # classes._set_child('spect_energy_budg', attribs=attribs)
-
-        # attribs = {
-        #     'module_name': 'fluidsim.base.output.increments',
-        #     'class_name': 'Increments'}
-        # classes._set_child('increments', attribs=attribs)
 
     # @staticmethod
     # def _complete_params_with_default(params, info_solver):
