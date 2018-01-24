fa38e45c2d99f9af255f5c03feec0574a7cb3b9b
calpe <miguel.calpe-linares@univ-grenoble-alpes.fr>
2018-01-24 13:40:41 +0100

    new class InitFieldsLinearMode solver ns2d.strat

--- fluidsim/solvers/ns2d/strat/init_fields.py
+++ fluidsim/solvers/ns2d/strat/init_fields.py
@@ -1,32 +1,157 @@
 """Initialization of the field (:mod:`fluidsim.solvers.ns2d.strat.init_fields`)
 ===============================================================================
 
-.. autoclass:: InitFieldsNS2D
+.. autoclass:: InitFieldsNS2DStrat
    :members:
 
-.. autoclass:: InitFieldsNoise
+.. autoclass:: InitFieldsNoiseStrat
    :members:
 
-.. autoclass:: InitFieldsJet
+.. autoclass:: InitFieldsJetStrat
    :members:
 
-.. autoclass:: InitFieldsDipole
+.. autoclass:: InitFieldsDipoleStrat
    :members:
 
 """
 
+from __future__ import print_function
+
 import numpy as np
 
 from fluiddyn.util import mpi
+from past.utils import old_div
 
 from fluidsim.base.init_fields import InitFieldsBase, SpecificInitFields
-
-from fluidsim.solvers.ns2d.init_fields import InitFieldsNS2D, InitFieldsNoise, InitFieldsJet, InitFieldsDipole
-
-InitFieldsNS2DStrat = InitFieldsNS2D
-
-InitFieldsNoiseStrat = InitFieldsNoise
+from fluidsim.solvers.ns2d.init_fields import InitFieldsNoise, \
+    InitFieldsJet, InitFieldsDipole
 
 InitFieldsJetStrat = InitFieldsJet
-
 InitFieldsDipoleStrat = InitFieldsDipole
+
+# It would be nice that rot_fft, ux_fft and uy_fft are attributes of the class
+# init_fields.
+
+
+class InitFieldsNoiseStrat(InitFieldsNoise):
+    """
+    Class to initialize the fields with noise for the solver ns2d.strat.
+
+    """
+
+    def compute_rotuxuy_fft(self):
+        """
+        Compute the rot_fft, ux_fft and uy_fft from a random noise field.
+        """
+        params = self.sim.params
+        oper = self.sim.oper
+
+        lambda0 = params.init_fields.noise.length
+        if lambda0 == 0:
+            lambda0 = old_div(oper.Lx, 4)
+
+        def H_smooth(x, delta):
+            return old_div((1. + np.tanh(2*np.pi*x/delta)), 2)
+
+        # to compute always the same field... (for 1 resolution...)
+        np.random.seed(42)  # this does not work for MPI...
+
+        ux_fft = (np.random.random(oper.shapeK) +
+                  1j*np.random.random(oper.shapeK) - 0.5 - 0.5j)
+        uy_fft = (np.random.random(oper.shapeK) +
+                  1j*np.random.random(oper.shapeK) - 0.5 - 0.5j)
+
+        if mpi.rank == 0:
+            ux_fft[0, 0] = 0.
+            uy_fft[0, 0] = 0.
+
+        oper.projection_perp(ux_fft, uy_fft)
+        oper.dealiasing(ux_fft, uy_fft)
+
+        k0 = 2*np.pi/lambda0
+        delta_k0 = 1.*k0
+        ux_fft = ux_fft*H_smooth(k0-oper.KK, delta_k0)
+        uy_fft = uy_fft*H_smooth(k0-oper.KK, delta_k0)
+
+        ux = oper.ifft2(ux_fft)
+        uy = oper.ifft2(uy_fft)
+        velo_max = np.sqrt(ux**2+uy**2).max()
+        if mpi.nb_proc > 1:
+            velo_max = oper.comm.allreduce(velo_max, op=mpi.MPI.MAX)
+        ux = params.init_fields.noise.velo_max*ux/velo_max
+        uy = params.init_fields.noise.velo_max*uy/velo_max
+        ux_fft = oper.fft2(ux)
+        uy_fft = oper.fft2(uy)
+
+        # if NO_SHEAR_MODES --> No energy in shear modes!
+        if self.sim.params.NO_SHEAR_MODES:
+            ux_fft[0, :] = 0
+            uy_fft[0, :] = 0
+
+        rot_fft = oper.rotfft_from_vecfft(ux_fft, uy_fft)
+        return rot_fft, ux_fft, uy_fft
+
+
+class InitFieldsLinearMode(SpecificInitFields):
+    """
+    Class to initialize the fields with the linear mode
+    """
+    tag = 'linear_mode'
+
+    @classmethod
+    def _complete_params_with_default(cls, params):
+        """Complete the `params` container (class method)."""
+        super(InitFieldsLinearMode, cls)._complete_params_with_default(params)
+        params.init_fields._set_child(cls.tag, attribs={
+            'i_mode': (8, 8),
+            'epsilon': 1})
+
+    def __call__(self):
+        ap_fft, am_fft = self.compute_linear_mode()
+        self.sim.state.init_from_linear_mode(ap_fft, am_fft)
+
+    def compute_linear_mode(self):
+        """Compute the linear mode ap_fft, am_fft."""
+
+        params = self.sim.params
+        oper = self.sim.oper
+
+        i_mode = params.init_fields.linear_mode.i_mode
+        epsilon = params.init_fields.linear_mode.epsilon
+
+        am_fft = np.zeros(oper.shapeK) + 1j * np.zeros(oper.shapeK)
+        ap_fft = am_fft.copy()
+
+        # Define contour epsilon region [epsx_min: epsx_max, epsy_min, epsymax]
+        epsx_min = i_mode[0] - epsilon
+        epsy_min = i_mode[1] - epsilon
+
+        epsx_max = i_mode[0] + epsilon + 1
+        epsy_max = i_mode[1] + epsilon + 1
+
+        # print('epsx_min = ', epsx_min)
+        # print('epsx_max = ', epsx_max)
+        # print('epsy_min = ', epsy_min)
+        # print('epsy_max = ', epsy_max)
+
+        if epsx_min < 0:
+            epsx_min = 0
+        if epsy_min < 0:
+            epsy_min = 0
+
+        ap_fft[epsx_min:epsx_max, epsy_min:epsy_max] = 1. + 0j
+        # print('ap_fft = ', ap_fft)
+        oper.dealiasing(am_fft, ap_fft)
+        # print('ap_fft_dealiasing = ', ap_fft)
+        return ap_fft, am_fft
+
+
+class InitFieldsNS2DStrat(InitFieldsBase):
+    """Initialize the state for the solver ns2d.strat."""
+
+    @staticmethod
+    def _complete_info_solver(info_solver):
+        """Complete the `info_solver` container (static method)."""
+        InitFieldsBase._complete_info_solver(
+            info_solver, classes=[InitFieldsNoiseStrat, InitFieldsJetStrat,
+                                  InitFieldsDipoleStrat, InitFieldsLinearMode])
