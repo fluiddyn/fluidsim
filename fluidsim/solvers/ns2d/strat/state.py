b63e02fae15962447135923115b46135b28b7c81
calpe <miguel.calpe.linares@gmail.com>
2018-02-19 14:01:07 +0100

    keys_computable + init_from_apfft + _compute_state_from_apfft

--- fluidsim/solvers/ns2d/strat/state.py
+++ fluidsim/solvers/ns2d/strat/state.py
@@ -31,7 +31,8 @@ class StateNS2DStrat(StateNS2D):
         info_solver.classes.State._set_attribs({
             'keys_state_spect': ['rot_fft', 'b_fft'],
             'keys_state_phys': ['ux', 'uy', 'rot', 'b'],
-            'keys_computable': [],
+            'keys_computable': ['ux_fft', 'uy_fft', 'div_fft', 'div', 'ap_fft',
+                                'am_fft'],
             'keys_phys_needed': ['rot', 'b'],
             'keys_linear_eigenmodes': ['rot_fft', 'b_fft']})
 
@@ -52,28 +53,29 @@ class StateNS2DStrat(StateNS2D):
             result = self.oper.fft2(self.state_phys.get_var('ux'))
         elif key == 'uy_fft':
             result = self.oper.fft2(self.state_phys.get_var('uy'))
-        elif key == 'rot_fft':
-            ux_fft = self.compute('ux_fft')
-            uy_fft = self.compute('uy_fft')
-            result = self.oper.rotfft_from_vecfft(ux_fft, uy_fft)
-
-        # Introduction buoyancy term 'b'
-        elif key == 'b_fft':
-            result = self.oper.fft2(self.state_phys.get_var('b'))
-
         elif key == 'div_fft':
             ux_fft = self.compute('ux_fft')
             uy_fft = self.compute('uy_fft')
             result = self.oper.divfft_from_vecfft(ux_fft, uy_fft)
-        elif key == 'rot':
-            rot_fft = self.compute('rot_fft')
-            result = self.oper.ifft2(rot_fft)
         elif key == 'div':
             div_fft = self.compute('div_fft')
             result = self.oper.ifft2(div_fft)
-        elif key == 'q':
-            rot = self.compute('rot')
-            result = rot
+        elif key == 'ap_fft':
+            uy_fft = self.oper.fft2(self.state_phys.get_var('uy'))
+            b_fft = self.state_spect.get_var('b_fft')
+
+            N = self.sim.params.N
+            omega_k = self.sim.compute_dispersion_relation()
+
+            result = (N**2) * uy_fft + j * omega_k * b_fft
+        elif key == 'am_fft':
+            uy_fft = self.oper.fft2(self.state_phys.get_var('uy'))
+            b_fft = self.state_spect.get_var('b_fft')
+
+            N = self.sim.params.N
+            omega_k = self.sim.compute_dispersion_relation()
+
+            result = (N**2) * uy_fft - j * omega_k * b_fft
         else:
             to_print = 'Do not know how to compute "' + key + '".'
             if RAISE_ERROR:
@@ -129,8 +131,17 @@ class StateNS2DStrat(StateNS2D):
 
         self.statephys_from_statespect()
 
-    def init_from_linear_mode(self, ap_fft, am_fft):
-        """Initialize state from the linear mode."""
+    def init_from_apfft(self, ap_fft):
+        """Initialize the state from the linear mode ap_fft. """
+        rot_fft, b_fft = self._compute_state_from_apfft(ap_fft)
+        self.init_from_rotbfft(rot_fft, b_fft)
+
+    def _compute_state_from_apfft(self, ap_fft):
+        """
+        Computes rot_fft and b_fft from linear mode ap_fft.
+        """
+        am_fft = np.zeros(self.oper.shapeK) + 1j * np.zeros(self.oper.shapeK)
+        
         uy_fft = (1. / (2 * self.params.N**2)) * (ap_fft + am_fft)
         cond = self.oper.KX != 0
         division = np.zeros_like(self.oper.KY)
@@ -143,17 +154,19 @@ class StateNS2DStrat(StateNS2D):
         b_fft = np.zeros_like(am_fft)
         b_fft[cond] = (1. / (2j * omega_k[cond])) * (
             ap_fft[cond] + am_fft[cond])
-        self.init_from_rotbfft(rot_fft, b_fft)
+        return rot_fft, b_fft
+        
 
     def init_from_rotfft(self, rot_fft):
         b_fft = np.zeros(self.oper.shapeK_loc, dtype=np.complex128)
         self.init_from_rotbfft(rot_fft, b_fft)
 
     def init_statespect_from(self, **kwargs):
-
-        # init_statespect_from looks if kwargs has two arguments.
         if len(kwargs) == 2:
             if 'rot_fft' in kwargs and 'b_fft' in kwargs:
                 self.init_from_rotbfft(kwargs['rot_fft'], kwargs['b_fft'])
+        elif len(kwargs) == 1:
+            if 'ap_fft' in kwargs:
+                self.init_from_apfft(kwargs['ap_fft'])
         else:
             super(StateNS2D, self).init_statespect_from(**kwargs)
