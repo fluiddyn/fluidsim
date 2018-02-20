aeb0e1418807f9cd5b3b6c168bb740fed1262464
calpe <miguel.calpe.linares@gmail.com>
2018-02-20 14:44:14 +0100

    test_spectra solver ns2d.strat

--- fluidsim/solvers/ns2d/strat/output/test/test_spect_energy_budg.py
+++ fluidsim/solvers/ns2d/strat/output/test/test_spect_energy_budg.py
@@ -15,16 +15,17 @@ class TestNS2DStrat(BaseTestCase):
         nh = 32
         super(TestNS2DStrat, cls).setUpClass(nh=nh, init_fields=init_fields,
                                              type_forcing=type_forcing,
-                                             HAS_TO_SAVE=True,
+                                             HAS_TO_SAVE=False,
                                              forcing_enable=False)
 
     def test_nonlinear_transfer(self):
         """
         Check sum(transferEK_kx) == 0
         """
-        oper = self.sim.oper
-        state_phys = self.sim.state.state_phys
-        state_spect = self.sim.state.state_spect
+        sim = self.sim
+        oper = sim.oper
+        state_phys = sim.state.state_phys
+        state_spect = sim.state.state_spect
 
         ux = state_phys.get_var('ux')
         uy = state_phys.get_var('uy')
@@ -58,7 +59,39 @@ class TestNS2DStrat(BaseTestCase):
         sum_transferEK_kx = transferEK_kx.sum()
         self.assertAlmostEqual(sum_transferEK_kx, 0)
 
+    def test_spectra(self):
+        """
+        Check sum(spectra) * deltak == energy.
+        """
+        sim = self.sim
+        oper = sim.oper
+        state_spect = sim.state.state_spect
 
+        rot_fft = state_spect.get_var('rot_fft')
+        b_fft = state_spect.get_var('b_fft')
+        ux_fft, uy_fft = oper.vecfft_from_rotfft(rot_fft)
+        
+        energyK_fft = (1. / 2) * (np.abs(ux_fft.conj() * ux_fft) +
+                                  np.abs(uy_fft.conj() * uy_fft))
+        spectrum1DEK_kx, spectrum1DEK_ky = oper.spectra1D_from_fft(energyK_fft)
+        energyK = oper.sum_wavenumbers(energyK_fft)
+        self.assertAlmostEqual(spectrum1DEK_kx.sum() * oper.deltakx, energyK)
+        self.assertAlmostEqual(spectrum1DEK_ky.sum() * oper.deltaky, energyK)
+
+        try:
+            energyA_fft = (1. / 2) * ((
+                np.abs(b_fft.conj() * b_fft) / sim.params.N**2))
+        except ZeroDivisionError:
+            pass
+        else:
+            spectrum1DEA_kx, spectrum1DEA_ky = oper.spectra1D_from_fft(
+                energyA_fft)
+            energyA = oper.sum_wavenumbers(energyA_fft)
+
+            self.assertAlmostEqual(
+                spectrum1DEA_kx.sum() * oper.deltakx, energyA)
+            self.assertAlmostEqual(
+                spectrum1DEA_ky.sum() * oper.deltaky, energyA)
         
 if __name__ == '__main__':
     unittest.main()        
