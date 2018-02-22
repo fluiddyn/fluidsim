9d4d8046e0718193c8f6db6be6062a7dab36bfa8
calpe <miguel.calpe.linares@gmail.com>
2018-02-22 09:54:32 +0100

    resolving conflicts

--- fluidsim/base/output/phys_fields.py
+++ fluidsim/base/output/phys_fields.py
@@ -265,8 +265,7 @@ def time_from_path(path):
 class MoviesBasePhysFields2D(MoviesBase2D):
     """Methods required to animate physical fields HDF5 files."""
 
-    def _ani_init(self, key_field, numfig, dt_equations, tmin, tmax,
-                  quiver_sep=None, INLET_ANIMATION=True, **kwargs):
+    def _ani_init(self, key_field, numfig, dt_equations, tmin, tmax, **kwargs):
         """Initialize list of files and times, pcolor plot, quiver and colorbar.
         """
         self._set_path()
@@ -277,6 +276,7 @@ class MoviesBasePhysFields2D(MoviesBase2D):
 
         if dt_equations is None:
             dt_equations = np.median(np.diff(self._ani_t_actual))
+            print('dt_equations = {:.4f}'.format(dt_equations))
 
         if tmax is None:
             tmax = self._ani_t_actual.max()
@@ -289,38 +289,48 @@ class MoviesBasePhysFields2D(MoviesBase2D):
         if dt_equations < dt_file / 4:
             raise ValueError('dt_equations < dt_file / 4')
 
+        self._has_uxuy = self.sim.state.has_vars('ux', 'uy')
         field, ux, uy = self._ani_get_field(0)
-        self._ani_init_fig(field, ux, uy, quiver_sep, INLET_ANIMATION)
+
+        INSET = True if 'INSET' not in kwargs else kwargs['INSET']
+        self._ani_init_fig(field, ux, uy, INSET)
         self._ani_clim = kwargs.get('clim')
         self._ani_set_clim()
 
-    def _ani_init_fig(self, field, ux, uy, quiver_sep, INLET_ANIMATION):
+    def _ani_init_fig(self, field, ux=None, uy=None, INSET=True):
+        """Initialize only the figure and related matplotlib objects. This
+        method is shared by both ``animate`` and ``online_plot``
+        functionalities.
 
-        x, y = self._select_axis(shape=ux.shape)
+        """
+        x, y = self._select_axis(shape=field.shape)
         XX, YY = np.meshgrid(x, y)
 
         self._ani_im = self._ani_ax.pcolor(XX, YY, field)
         self._ani_cbar = self._ani_fig.colorbar(self._ani_im)
-        self._ani_quiver, vmax = self._quiver_plot(
-            self._ani_ax, quiver_sep, ux, uy, XX, YY)
+
+        self._has_uxuy = self.sim.state.has_vars('ux', 'uy')
+
+        if self._has_uxuy:
+            self._ani_quiver, vmax = self._quiver_plot(
+                self._ani_ax, ux, uy, XX, YY)
 
         if not self.sim.time_stepping.is_simul_completed():
-            INLET_ANIMATION = False
+            INSET = False
 
         try:
             self.output.spatial_means
         except AttributeError:
-            INLET_ANIMATION = False
+            INSET = False
 
-        self._ANI_INLET_ANIMATION = INLET_ANIMATION
-
-        if self._ANI_INLET_ANIMATION:
+        self._ANI_INSET = INSET
+        if self._ANI_INSET:
             try:
                 self._ani_spatial_means_t, self._ani_spatial_means_key = (
                     self._get_spatial_means())
             except FileNotFoundError:
-                print('No spatial means file => no inlet plot.')
-                self._ANI_INLET_ANIMATION = False
+                print('No spatial means file => no inset plot.')
+                self._ANI_INSET = False
                 return
 
             left, bottom, width, height = [0.53, 0.67, 0.2, 0.2]
@@ -333,7 +343,7 @@ class MoviesBasePhysFields2D(MoviesBase2D):
             ax2.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
 
             ax2.set_xlim(0, self._ani_spatial_means_t.max())
-            # Correct visualization inlet_animation 10% of the difference
+            # Correct visualization inset_animation 10% of the difference
             # value_max-value-min
             ax2.set_ylim(
                 self._ani_spatial_means_key.min(),
@@ -344,9 +354,9 @@ class MoviesBasePhysFields2D(MoviesBase2D):
             ax2.plot(
                 self._ani_spatial_means_t, self._ani_spatial_means_key,
                 linewidth=0.8, color='grey', alpha=0.4)
-            self._ani_im_inlet = ax2.plot([0], [0], color='red')
+            self._ani_im_inset = ax2.plot([0], [0], color='red')
 
-    def _quiver_plot(self, ax, quiver_sep, vecx='ux', vecy='uy', XX=None, YY=None):
+    def _quiver_plot(self, ax, vecx='ux', vecy='uy', XX=None, YY=None):
         '''Make a quiver plot on axis `ax`.'''
         pass
 
@@ -372,7 +382,7 @@ class MoviesBasePhysFields2D(MoviesBase2D):
         with h5py.File(self._ani_pathfiles[idx]) as f:
             field = f['state_phys'][key].value
 
-            if need_uxuy:
+            if need_uxuy and self._has_uxuy:
                 try:
                     ux = f['state_phys']['ux'].value
                     uy = f['state_phys']['uy'].value
@@ -381,7 +391,10 @@ class MoviesBasePhysFields2D(MoviesBase2D):
                     uy = f['state_phys']['vy'].value
 
         if need_uxuy:
-            return field, ux, uy
+            if self._has_uxuy:
+                return field, ux, uy
+            else:
+                return field, None, None
         else:
             return field
 
@@ -395,24 +408,29 @@ class MoviesBasePhysFields2D(MoviesBase2D):
 
         # Update figure, quiver and colorbar
         self._ani_im.set_array(field.flatten())
-        vmax = np.max(np.sqrt(ux ** 2 + uy ** 2))
-        self._ani_quiver.set_UVC(ux[::self._skip, ::self._skip],
-                                 uy[::self._skip, ::self._skip])
+        if self._has_uxuy:
+            vmax = np.max(np.sqrt(ux ** 2 + uy ** 2))
+            self._ani_quiver.set_UVC(ux[::self._skip, ::self._skip]/vmax,
+                                     uy[::self._skip, ::self._skip]/vmax)
+        else:
+            vmax = None
+
         self._ani_im.autoscale()
         self._ani_set_clim()
 
         # INLET ANIMATION
-        if self._ANI_INLET_ANIMATION:
+        if self._ANI_INSET:
             idx_spatial = np.abs(self._ani_spatial_means_t - time).argmin()
             t = self._ani_spatial_means_t
             E = self._ani_spatial_means_key
 
-            self._ani_im_inlet[0].set_data(
+            self._ani_im_inset[0].set_data(
                 t[:idx_spatial], E[:idx_spatial])
 
         self._set_title(self._ani_ax, self._ani_key, time, vmax)
 
     def _set_title(self, ax, key, time, vmax=None):
+        # print('time={}'.format(time))
         title = (key +
                  ', $t = {0:.3f}$, '.format(time) +
                  self.output.name_solver +
@@ -430,287 +448,3 @@ class MoviesBasePhysFields2D(MoviesBase2D):
             self._ani_cbar.set_clim(*clim)
             ticks = np.linspace(*clim, num=21, endpoint=True)
             self._ani_cbar.set_ticks(ticks)
-
-    def _ani_get_weighted_field(self, time):
-        """Get weighted field between to saved files."""
-        idx, t_actual = self._ani_get_t_actual(time)
-        # If time > time last frame --> frame == last frame.
-        if idx + 1 >= len(self._ani_t_actual) and time > t_actual:
-            t = self._ani_t_actual[idx - 1]
-            field, ux, uy = self._ani_get_field(t)
-
-        else:
-            if t_actual < time:
-                dt_save = self._ani_t_actual[idx + 1] - self._ani_t_actual[idx]
-                weight_0 = 1 - np.abs(
-                    time - self._ani_t_actual[idx]) / dt_save
-                weight_1 = 1 - np.abs(
-                    time - self._ani_t_actual[idx + 1]) / dt_save
-
-                t0 = self._ani_t_actual[idx]
-                field_0, ux_0, uy_0 = self._ani_get_field(t0)
-
-                t1 = self._ani_t_actual[idx + 1]
-                field_1, ux_1, uy_1 = self._ani_get_field(t1)
-
-                field = field_0 * weight_0 + field_1 * weight_1
-                ux = ux_0 * weight_0 + ux_1 * weight_1
-                uy = uy_0 * weight_0 + uy_1 * weight_1
-
-            elif t_actual > time:
-
-                dt_save = self._ani_t_actual[idx] - self._ani_t_actual[idx - 1]
-                weight_0 = 1 - np.abs(
-                    time - self._ani_t_actual[idx - 1]) / dt_save
-                weight_1 = 1 - np.abs(
-                    time - self._ani_t_actual[idx]) / dt_save
-
-                t0 = self._ani_t_actual[idx - 1]
-                field_0, ux_0, uy_0 = self._ani_get_field(t0)
-
-                t1 = self._ani_t_actual[idx]
-                field_1, ux_1, uy_1 = self._ani_get_field(t1)
-
-                field = field_0 * weight_0 + field_1 * weight_1
-                ux = ux_0 * weight_0 + ux_1 * weight_1
-                uy = uy_0 * weight_0 + uy_1 * weight_1
-
-            else:
-                t = self._ani_t_actual[idx]
-                field, ux, uy = self._ani_get_field(t)
-
-        return field, ux, uy
-
-    def _get_spatial_means(self, key_spatial='E'):
-        """ Get field for the inlet plot."""
-        # Check if key_spatial can be loaded.
-        keys_spatial = ['E', 'EK', 'EA']
-        if key_spatial not in keys_spatial:
-            raise ValueError('key_spatial not in spatial means keys.')
-        # Load data for inlet plot
-        dico = self.output.spatial_means.load()
-        t = dico['t']
-        E = dico[key_spatial]
-
-        return t, E
-
-
-class PhysFieldsBase2D(PhysFieldsBase, MoviesBasePhysFields2D):
-
-    def _init_online_plot(self):
-        self._ani_key = self.params.output.phys_fields.field_to_plot
-        self._ani_fig, self._ani_ax = plt.subplots()
-        self._set_font()
-        field, _ = self._select_field(key_field=self._ani_key)
-        ux, _ = self._select_field(key_field='ux')
-        uy, _ = self._select_field(key_field='uy')
-        if mpi.rank == 0:
-            self._ani_init_fig(field, ux, uy)
-            self._ani_im.autoscale()
-            # self._ani_im.draw()
-
-    def _online_plot(self):
-        """Online plot."""
-        tsim = self.sim.time_stepping.t
-        if (tsim - self.t_last_plot >= self.period_plot):
-            self.t_last_plot = tsim
-            key_field = self.params.output.phys_fields.field_to_plot
-            field, _ = self._select_field(key_field=key_field)
-            ux, _ = self._select_field(key_field='ux')
-            uy, _ = self._select_field(key_field='uy')
-            if mpi.rank == 0:
-                field = field[:-1, :-1]
-
-                # Update figure, quiver and colorbar
-                self._ani_im.set_array(field.flatten())
-                vmax = np.max(np.sqrt(ux ** 2 + uy ** 2))
-                self._ani_quiver.set_UVC(ux[::self._skip, ::self._skip] / vmax,
-                                         uy[::self._skip, ::self._skip] / vmax)
-                self._set_title(self._ani_ax, self._ani_key, tsim, vmax)
-
-                self._ani_im.autoscale()
-                # self._ani_im.draw()
-                self._ani_fig.canvas.draw()
-                plt.pause(1e-6)
-
-    def plot(self, field=None, key_field=None, time=None,
-             QUIVER=True, vecx='ux', vecy='uy',
-             nb_contours=20, type_plot='contourf', iz=0, vmin=None, vmax=None,
-             cmap='viridis', numfig=None, quiver_sep=None):
-        """Plot a field.
-
-        This function is surely buggy! It has to be fixed.
-
-        Parameters
-        ----------
-
-        field : {str, np.ndarray}, optional
-
-        time : number, optional
-
-        QUIVER : True
-
-        vecx : 'ux'
-
-        vecy : 'uy'
-
-        nb_contours : 20
-
-        type_plot : 'contourf'
-
-        iz : 0
-
-        vmin : None
-
-        vmax : None
-
-        cmap : 'viridis'
-
-        numfig : None
-
-        quiver_sep : None
-          Percentage of Lx to compute separation arrows.
-
-        """
-
-        is_field_ready = False
-
-        if field is None and key_field is None:
-            key_field = self.field_to_plot
-        elif isinstance(field, np.ndarray) and key_field is None:
-            key_field = 'given array'
-            is_field_ready = True
-        elif field is not None and key_field is None:
-            key_field, field = field, key_field
-
-        assert key_field is not None
-
-        if time is None and not is_field_ready:
-            # we have to get the field from the state
-            time = self.sim.time_stepping.t
-            field = self._get_field_seq(key_field)
-            if QUIVER:
-                vecx = self._get_field_seq(vecx)
-                vecy = self._get_field_seq(vecy)
-        else:
-            # we have to get the field from a file
-            self._set_path()
-
-            if key_field not in self.sim.state.keys_state_phys:
-                raise ValueError('key not in state.keys_state_phys')
-
-            self._ani_pathfiles = sorted(glob(os.path.join(
-                self.path, 'state_phys*')))
-            self._ani_t_actual = np.array(list(
-                map(time_from_path, self._ani_pathfiles)))
-
-            idx, t_actual = self._ani_get_t_actual(time)
-            time = t_actual
-
-            keys_state_phys = self.sim.state.keys_state_phys
-            if vecx not in keys_state_phys or vecy not in keys_state_phys:
-                QUIVER = False
-
-            if QUIVER:
-                field, vecx, vecy = self._ani_get_field(time, key_field)
-            else:
-                field = self._ani_get_field(time, key_field, need_uxuy=False)
-
-        if field.ndim == 3:
-            field = field[iz]
-            vecx, vecy = vecx[iz], vecy[iz]
-
-        if mpi.rank == 0:
-            if numfig is None:
-                fig, ax = self.output.figure_axe()
-            else:
-                fig, ax = self.output.figure_axe(numfig=numfig)
-            x_seq = self.oper.x_seq
-            y_seq = self.oper.y_seq
-            [XX_seq, YY_seq] = np.meshgrid(x_seq, y_seq)
-            try:
-                cmap = plt.get_cmap(cmap)
-            except ValueError:
-                print('Use matplotlib >= 1.5.0 for new standard colorschemes.\
-                       Installed matplotlib :' + plt.matplotlib.__version__)
-                cmap = plt.get_cmap('jet')
-
-            if type_plot == 'contourf':
-                contours = ax.contourf(
-                    x_seq, y_seq, field,
-                    nb_contours, vmin=vmin, vmax=vmax, cmap=cmap)
-                fig.colorbar(contours)
-                fig.contours = contours
-            elif type_plot == 'pcolor':
-                pc = ax.pcolormesh(x_seq, y_seq, field,
-                                   vmin=vmin, vmax=vmax, cmap=cmap)
-                fig.colorbar(pc)
-        else:
-            ax = None
-
-        if QUIVER:
-            quiver, vmax = self._quiver_plot(ax, quiver_sep, vecx, vecy)
-        else:
-            vmax = None
-
-        if mpi.rank == 0:
-            ax.set_xlabel('x')
-            ax.set_ylabel('y')
-            self._set_title(ax, key_field, time, vmax)
-
-            fig.tight_layout()
-            fig.canvas.draw()
-            plt.pause(1e-3)
-
-    def _compute_skip_quiver(self, quiver_sep):
-        """
-        Computes the skip for the quiver. quiver_sep is the 
-        percentage of Lx for the arrows separation.
-
-        """
-        if quiver_sep is None:
-            # 4% of the Lx it is a great separation between vector arrows.
-            quiver_sep = 0.04
-        delta_quiver = quiver_sep * self.oper.Lx
-        skip = (self.oper.nx_seq / self.oper.Lx) * delta_quiver
-        skip = int(np.round(skip))
-        if skip < 1:
-            skip = 1
-        return skip
-
-    def _get_field_seq(self, key):
-        field_loc = self.sim.state.get_var(key)
-        if mpi.nb_proc > 1:
-            return self.oper.gather_Xspace(field_loc)
-        else:
-            return field_loc
-
-    def _quiver_plot(self, ax, quiver_sep, vecx='ux', vecy='uy', XX=None,
-                     YY=None):
-        """Superimposes a quiver plot of velocity vectors with a given axis
-        object corresponding to a 2D contour plot.
-
-        """
-        if isinstance(vecx, basestring):
-            vecx = self._get_field_seq(vecx)
-
-        if isinstance(vecy, basestring):
-            vecy = self._get_field_seq(vecy)
-
-        self._skip = skip = self._compute_skip_quiver(quiver_sep)
-
-        if XX is None and YY is None:
-            [XX, YY] = np.meshgrid(self.oper.x_seq, self.oper.y_seq)
-
-        if mpi.rank == 0:
-            vmax = np.max(np.sqrt(vecx**2 + vecy**2))
-            # copy to avoid a bug
-            vecx_c = vecx[::skip, ::skip].copy()
-            vecy_c = vecy[::skip, ::skip].copy()
-            # Scale quiver is found to be good 10*vmax of each field.
-            quiver = ax.quiver(
-                XX[::skip, ::skip],
-                YY[::skip, ::skip],
-                vecx_c, vecy_c, scale=10*vmax)
-
-        return quiver, vmax
