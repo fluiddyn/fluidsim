0d16d60deb230d6f0c34579542b72222262d86a0
calpe <miguel.calpe.linares@gmail.com>
2018-03-02 14:44:46 +0100

    Print ux_fft to see bug.

--- fluidsim/solvers/ns2d/init_fields.py
+++ fluidsim/solvers/ns2d/init_fields.py
@@ -39,6 +39,7 @@ class InitFieldsNoise(SpecificInitFields):
 
     def __call__(self):
         rot_fft, ux_fft, uy_fft = self.compute_rotuxuy_fft()
+        energy_fft = 0.5 * (np.abs(ux_fft)**2 + np.abs(uy_fft)**2)
         self.sim.state.init_from_rotfft(rot_fft)
 
     def compute_rotuxuy_fft(self):
@@ -78,10 +79,13 @@ class InitFieldsNoise(SpecificInitFields):
         velo_max = np.sqrt(ux**2+uy**2).max()
         if mpi.nb_proc > 1:
             velo_max = oper.comm.allreduce(velo_max, op=mpi.MPI.MAX)
+        # print('velo_max = ', velo_max)
         ux = params.init_fields.noise.velo_max*ux/velo_max
         uy = params.init_fields.noise.velo_max*uy/velo_max
+
         ux_fft = oper.fft2(ux)
         uy_fft = oper.fft2(uy)
+        print('ux_fft', ux_fft)
 
         rot_fft = oper.rotfft_from_vecfft(ux_fft, uy_fft)
         return rot_fft, ux_fft, uy_fft
