29494c371960270a324dc74a78e6ac15f2cc636c
calpe <miguel.calpe.linares@gmail.com>
2018-03-22 15:38:42 +0100

    Test energy conservation in spatial means.

--- fluidsim/solvers/ns2d/strat/output/test/test_spatial_means.py
+++ fluidsim/solvers/ns2d/strat/output/test/test_spatial_means.py
@@ -9,6 +9,19 @@ from fluidsim.solvers.ns2d.strat.output.test import TestNS2DStrat, mpi
 class TestSpatialMeans(TestNS2DStrat):
     _tag = 'spatial_means'
 
+    def test_energy_conservation(self):
+        """Check conservation of energy (no viscosity & no dissipation)"""
+        dico = self.dico
+        energies = dico['E']
+
+        # Compute energy difference
+        dE = []
+        for i in range(len(energies)-1):
+            difference = abs(energies[i+1] - energies[i])
+            dE.append(difference)
+
+        self.assertAlmostEqual(np.mean(dE), 0)
+
     @unittest.skipIf(mpi.nb_proc > 1,
                      'plot function works sequentially only')
     def test_plot_spatial_means(self):
