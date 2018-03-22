78fb03a47739ff8bad099ccd9754019f46fb2fb7
calpe <miguel.calpe.linares@gmail.com>
2018-03-22 13:27:04 +0100

    Class TestSpectra for solver NS2DStrat.

---
+++ fluidsim/solvers/ns2d/strat/output/test/test_spectra.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+
+import unittest
+import numpy as np
+
+from fluidsim.solvers.ns2d.strat.output.test import TestNS2DStrat
+
+
+class TestSpectra(TestNS2DStrat):
+    _tag = 'spectra'
+    
+    def test_spectra(self):
+        """
+        Check sum(spectra) * deltak == energy.
+        """
+        sim = self.sim
+        oper = sim.oper
+        state_spect = sim.state.state_spect
+
+        rot_fft = state_spect.get_var('rot_fft')
+        b_fft = state_spect.get_var('b_fft')
+        ux_fft, uy_fft = oper.vecfft_from_rotfft(rot_fft)
+        
+        energyK_fft = (1. / 2) * (np.abs(ux_fft.conj() * ux_fft) +
+                                  np.abs(uy_fft.conj() * uy_fft))
+        spectrum1DEK_kx, spectrum1DEK_ky = oper.spectra1D_from_fft(energyK_fft)
+        energyK = oper.sum_wavenumbers(energyK_fft)
+        self.assertAlmostEqual(spectrum1DEK_kx.sum() * oper.deltakx, energyK)
+        self.assertAlmostEqual(spectrum1DEK_ky.sum() * oper.deltaky, energyK)
+
+        try:
+            energyA_fft = (1. / 2) * ((
+                np.abs(b_fft.conj() * b_fft) / sim.params.N**2))
+        except ZeroDivisionError:
+            pass
+        else:
+            spectrum1DEA_kx, spectrum1DEA_ky = oper.spectra1D_from_fft(
+                energyA_fft)
+            energyA = oper.sum_wavenumbers(energyA_fft)
+
+            self.assertAlmostEqual(
+                spectrum1DEA_kx.sum() * oper.deltakx, energyA)
+            self.assertAlmostEqual(
+                spectrum1DEA_ky.sum() * oper.deltaky, energyA)
+
+
+if __name__ == '__main__':
+    unittest.main()        
