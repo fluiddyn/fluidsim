4335fa67d55ce1f67f55418165fff4105163c324
calpe <miguel.calpe.linares@gmail.com>
2018-03-22 17:35:43 +0100

    Clean up __init__.py

--- fluidsim/solvers/ns2d/strat/output/test/__init__.py
+++ fluidsim/solvers/ns2d/strat/output/test/__init__.py
@@ -8,13 +8,15 @@ from fluidsim.solvers.sw1l.output.test import BaseTestCase, mpi
 class TestNS2DStrat(BaseTestCase):
     solver = 'NS2D.strat'
     _tag = ''
-
+    options = {'nh' : 32,
+               'init_fields' : 'noise',
+               'type_forcing' : 'tcrandom_anisotropic',
+               'HAS_TO_SAVE' : True,
+               'forcing_enable' : False,
+               'dissipation_enable' : False,
+               'periods_save_spatial_means' : 0.25}
+    
     @classmethod
-    def setUpClass(cls, init_fields='noise',
-                   type_forcing='tcrandom_anisotropic'):
-        nh = 128
-        super(TestNS2DStrat, cls).setUpClass(nh=nh, init_fields=init_fields,
-                                             type_forcing=type_forcing,
-                                             HAS_TO_SAVE=True,
-                                             forcing_enable=False,
-                                             dissipation_enable=False)
+    def setUpClass(cls):
+        super(TestNS2DStrat, cls).setUpClass(**cls.options)
+        
