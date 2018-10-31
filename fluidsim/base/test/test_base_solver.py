6d3899be351f3095fbe70a796f7c067dba5841c3
Ashwin Vishnu <avmo@kth.se>
2018-10-31 16:08:42 +0100

    Bring back stdout_redirected in fluidsim/base/test/test_base_solver.py

--- fluidsim/base/test/test_base_solver.py
+++ fluidsim/base/test/test_base_solver.py
@@ -2,6 +2,7 @@ import unittest
 import shutil
 
 import fluiddyn as fld
+from fluiddyn.io import stdout_redirected
 from fluiddyn.util import mpi
 
 # to get fld.show
@@ -17,10 +18,11 @@ class TestBaseSolver(unittest.TestCase):
         params.short_name_type_run = "test_base_solver"
         params.time_stepping.USE_CFL = False
         params.time_stepping.USE_T_END = False
-        params.time_stepping.it_end = 4
+        params.time_stepping.it_end = 2
         params.time_stepping.deltat0 = 0.1
 
-        self.sim = Simul(params)
+        with stdout_redirected():
+            self.sim = Simul(params)
 
     def tearDown(self):
         # clean by removing the directory
@@ -31,7 +33,8 @@ class TestBaseSolver(unittest.TestCase):
     @unittest.skipIf(mpi.nb_proc > 1, "Od solvers do not work with mpi")
     def test_simul(self):
         """Should be able to run a base experiment."""
-        self.sim.time_stepping.start()
+        with stdout_redirected():
+            self.sim.time_stepping.start()
 
         fld.show()
 
