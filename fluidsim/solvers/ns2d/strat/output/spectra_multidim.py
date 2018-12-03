67b6d758ef0a2387825d73b38df421d0c9cb9a87
calpe <miguel.calpe.linares@gmail.com>
2018-12-03 09:12:44 +0100

    Start working module spectra_multidim.py.

--- fluidsim/solvers/ns2d/strat/output/spectra_multidim.py
+++ fluidsim/solvers/ns2d/strat/output/spectra_multidim.py
@@ -7,8 +7,6 @@
 
 """
 
-from __future__ import print_function
-
 import h5py
 import numpy as np
 
@@ -20,20 +18,39 @@ class SpectraMultiDimNS2DStrat(SpectraMultiDim):
 
     def compute(self):
         """Computes multidimensional spectra at one time."""
+
+        # Get variables
         energyK_fft, energyA_fft = self.output.compute_energies_fft()
         energy_fft = energyK_fft + energyA_fft
+
+        ap_fft = self.sim.state.compute("ap_fft")
+        am_fft = self.sim.state.compute("am_fft")
 
         # Computes multidimensional spectra
         spectrumkykx_E = self.oper.compute_spectrum_kykx(energy_fft)
         spectrumkykx_EK = self.oper.compute_spectrum_kykx(energyK_fft)
         spectrumkykx_EA = self.oper.compute_spectrum_kykx(energyA_fft)
 
+        # The function compute_spectrum_kykx does not supports complex variable...
+        # Only works for the energy!
+
+        # spectrumkykx_ap_fft = self.oper.compute_spectrum_kykx(ap_fft)
+        # spectrumkykx_am_fft = self.oper.compute_spectrum_kykx(am_fft)
+
         # Saves dictionary
         dict_spectra = {
             "spectrumkykx_E": spectrumkykx_E,
             "spectrumkykx_EK": spectrumkykx_EK,
-            "spectrumkykx_EA": spectrumkykx_EA,
+            "spectrumkykx_EA": spectrumkykx_EA
         }
+
+        # dict_spectra = {
+        #     "spectrumkykx_E": spectrumkykx_E,
+        #     "spectrumkykx_EK": spectrumkykx_EK,
+        #     "spectrumkykx_EA": spectrumkykx_EA,
+        #     "spectrumkykx_ap_fft": spectrumkykx_ap_fft,
+        #     "spectrumkykx_am_fft": spectrumkykx_am_fft
+        # }
 
         return dict_spectra
 
