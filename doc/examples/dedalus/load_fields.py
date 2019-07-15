eb40d72a86f0d496d31e6bae66cbe4f885742079
paugier <pierre.augier@univ-grenoble-alpes.fr>
2019-07-15 16:36:07 +0200

    Buggy load_fields.py

--- doc/examples/dedalus/load_fields.py
+++ doc/examples/dedalus/load_fields.py
@@ -7,8 +7,8 @@ import fluidsim as fls
 paths = sorted((Path(fls.FLUIDSIM_PATH) / "examples").glob("dedalus_*"))
 path = paths[-1]
 
-sim = fls.load_sim_for_plot(path)
+sim = fls.load_state_phys_file(path)
 
-sim.output.phys_fields.plot("b")
+sim.output.phys_fields.plot("dz_b")
 
 plt.show()
