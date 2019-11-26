f7aa1b67c35085aedc9507d53fa1bc80af23d178
Jason Reneuve <jason.reneuve@univ-grenoble-alpes.fr>
2019-11-26 15:27:42 +0100

    updated submition script in doc/examples/internal_waves_coriolis

--- doc/examples/internal_waves_coriolis/submit_simul.py
+++ doc/examples/internal_waves_coriolis/submit_simul.py
@@ -1,15 +1,17 @@
-
 from fluiddyn.clusters.legi import Calcul8 as Cluster
+
 cluster = Cluster()
 
 cluster.commands_setting_env = [
     'source /etc/profile',
-    'export PATH="/home/users/augier3pi/opt/miniconda3/bin:$PATH"',
-    'export FLUIDSIM_PATH=/fsnet/project/watu/2018/18INTERNAL/Data_simul'
+    'export PATH="$HOME/miniconda3/bin:$PATH"'
+    'export FLUIDSIM_PATH=/fsnet/project/watu/2019/19INTSIM/sim_data'
 ]
 
-
 cluster.submit_script(
-    'simul_ns3dstrat_waves.py', name_run='fls_internal_waves',
-    nb_cores_per_node=cluster.nb_cores_per_node,
-    nb_mpi_processes=cluster.nb_cores_per_node, omp_num_threads=1)
+    'simul_ns3dstrat_waves.py',
+    name_run='fld_igw3d',
+    nb_cores_per_node=10,
+    nb_mpi_processes=10,
+    omp_num_threads=1,
+)
