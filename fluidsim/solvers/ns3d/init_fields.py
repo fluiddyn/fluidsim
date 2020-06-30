fluiddyn/fluidsim

commit 056c4daf3b87ce2e465b9fb867bc1ffa92a5dd8a

Simulations doc/examples/time_stepping/3d with noise

fluidsim/solvers/ns3d/init_fields.py
@@ -139,49 +139,54 @@ length: float (default 0.)
         self.sim.state.statephys_from_statespect()
 
     def compute_vv_fft(self):
+        params_noise = self.sim.params.init_fields.noise
+        return compute_solenoidal_noise_fft(
+            self.sim.oper, params_noise.length, params_noise.velo_max,
+        )
 
-        params = self.sim.params
-        oper = self.sim.oper
 
-        lambda0 = params.init_fields.noise.length
-        if lambda0 is None:
-            lambda0 = oper.Lx / 4.0
+def compute_solenoidal_noise_fft(oper, length=None, velo_max=1, seed=42):
+    """Compute a divergence-free (incompressible) random field"""
 
-        def H_smooth(x, delta):
-            return (1.0 + np.tanh(2 * np.pi * x / delta)) / 2.0
+    lambda0 = length
+    if lambda0 is None:
+        lambda0 = oper.Lx / 4.0
 
-        # to compute always the same field... (for 1 resolution...)
-        np.random.seed(42 + mpi.rank)
+    def H_smooth(x, delta):
+        return (1.0 + np.tanh(2 * np.pi * x / delta)) / 2.0
 
-        vv = [np.random.random(oper.shapeX_loc) - 0.5 for i in range(3)]
+    # to compute always the same field... (for 1 resolution...)
+    np.random.seed(seed + mpi.rank)
 
-        vv_fft = []
-        for ii, vi in enumerate(vv):
-            vv_fft.append(oper.fft(vi))
+    vv = [np.random.random(oper.shapeX_loc) - 0.5 for i in range(3)]
 
-            if mpi.rank == 0:
-                vv_fft[ii][0, 0, 0] = 0.0
+    vv_fft = []
+    for ii, vi in enumerate(vv):
+        vv_fft.append(oper.fft(vi))
 
-        oper.project_perpk3d(*vv_fft)
-        oper.dealiasing(*vv_fft)
+        if mpi.rank == 0:
+            vv_fft[ii][0, 0, 0] = 0.0
 
-        k0 = 2 * np.pi / lambda0
-        delta_k0 = 1.0 * k0
+    oper.project_perpk3d(*vv_fft)
+    oper.dealiasing(*vv_fft)
 
-        K = np.sqrt(oper.K2)
+    k0 = 2 * np.pi / lambda0
+    delta_k0 = 1.0 * k0
 
-        vv_fft = [vi_fft * H_smooth(k0 - K, delta_k0) for vi_fft in vv_fft]
-        vv = [oper.ifft(ui_fft) for ui_fft in vv_fft]
+    K = np.sqrt(oper.K2)
 
-        velo_max = np.sqrt(vv[0] ** 2 + vv[1] ** 2 + vv[2] ** 2).max()
-        if mpi.nb_proc > 1:
-            velo_max = oper.comm.allreduce(velo_max, op=mpi.MPI.MAX)
+    vv_fft = [vi_fft * H_smooth(k0 - K, delta_k0) for vi_fft in vv_fft]
+    vv = [oper.ifft(ui_fft) for ui_fft in vv_fft]
 
-        vv = [params.init_fields.noise.velo_max * vi / velo_max for vi in vv]
+    velo_max_result_random = np.sqrt(vv[0] ** 2 + vv[1] ** 2 + vv[2] ** 2).max()
+    if mpi.nb_proc > 1:
+        velo_max_result_random = oper.comm.allreduce(velo_max, op=mpi.MPI.MAX)
 
-        vv_fft = [oper.fft(vi) for vi in vv]
+    vv = [velo_max * vi / velo_max_result_random for vi in vv]
 
-        return tuple(vv_fft)
+    vv_fft = [oper.fft(vi) for vi in vv]
+
+    return tuple(vv_fft)
 
 
 class InitFieldsNS3D(InitFieldsBase):

doc/examples/time_stepping/3d/run_profile.py
@@ -45,7 +45,7 @@ def main(args):
     params.output.HAS_TO_SAVE = False
     params.output.periods_print.print_stdout = args.t_end / 4
     sim = Simul(params)
-    init_state(sim)
+    init_state(sim, args)
     Path("tmp_profile").mkdir(exist_ok=True)
     base_name_file = f"tmp_profile/{sim.name_run}."
     name_file_log = base_name_file + "log"

doc/examples/time_stepping/3d/run_simul.py
@@ -22,6 +22,7 @@ from fluiddyn.util import mpi
 
 from fluidsim.solvers.ns3d.solver import Simul
 from fluidsim import FLUIDSIM_PATH, load_for_restart
+from fluidsim.solvers.ns3d.init_fields import compute_solenoidal_noise_fft
 
 sub_directory = "taylor-green_phase-shifting"
 
@@ -82,10 +83,17 @@ parser.add_argument(
     default=None,
 )
 
-
 Re = 1600
 V0 = 1.0
 L = 1
+
+parser.add_argument(
+    "--velo_max_noise", help="noise level", type=float, default=V0 / 8,
+)
+
+parser.add_argument(
+    "--length_noise", help="noise length", type=float, default=L / 5,
+)
 
 
 def init_params(args):
@@ -143,7 +151,7 @@ def init_params(args):
     return params
 
 
-def init_state(sim):
+def init_state(sim, args):
     X, Y, Z = sim.oper.get_XYZ_loc()
 
     vx = V0 * np.sin(X / L) * np.cos(Y / L) * np.cos(Z / L)
@@ -151,8 +159,16 @@ def init_state(sim):
     vz = sim.oper.create_arrayX(value=0)
 
     sim.state.init_statephys_from(vx=vx, vy=vy, vz=vz)
-
     sim.state.statespect_from_statephys()
+
+    if args.velo_max_noise:
+        noise_fft = compute_solenoidal_noise_fft(
+            sim.oper, length=args.length_noise, velo_max=args.velo_max_noise
+        )
+        for i_direction, letter_direction in enumerate("xyz"):
+            vi_fft = sim.state.state_spect.get_var(f"v{letter_direction}_fft")
+            vi_fft += noise_fft[i_direction]
+
     sim.state.statephys_from_statespect()
 
 
@@ -167,7 +183,7 @@ def init_new_simul(args):
         sys.exit()
 
     sim = Simul(params)
-    init_state(sim)
+    init_state(sim, args)
     return params, sim
 
 

commit 67c6624f1a1337ea746bd6db5201a6472c91d225

Fix a silly bug in fluidsim/solvers/ns3d/init_fields.py introduced in 056c4daf3b87ce2e465b9fb867bc1ffa92a5dd8a

fluidsim/solvers/ns3d/init_fields.py
@@ -180,7 +180,9 @@ def compute_solenoidal_noise_fft(oper, length=None, velo_max=1, seed=42):
 
     velo_max_result_random = np.sqrt(vv[0] ** 2 + vv[1] ** 2 + vv[2] ** 2).max()
     if mpi.nb_proc > 1:
-        velo_max_result_random = oper.comm.allreduce(velo_max, op=mpi.MPI.MAX)
+        velo_max_result_random = oper.comm.allreduce(
+            velo_max_result_random, op=mpi.MPI.MAX
+        )
 
     vv = [velo_max * vi / velo_max_result_random for vi in vv]
 