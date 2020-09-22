deb2925a01340fc65a4df4dd269b48f9b2f5bc81
jreneuve <jason.reneuve@univ-grenoble-alpes.fr>
2020-09-23 01:02:03 +0200

    plot_mean function in phys_fields3d

--- fluidsim/base/output/phys_fields3d.py
+++ fluidsim/base/output/phys_fields3d.py
@@ -76,6 +76,7 @@ class PhysFieldsBase3D(PhysFieldsBase2D):
         vmax=None,
         cmap="viridis",
         numfig=None,
+        SCALED=True,
     ):
         """Plot a field.
 
@@ -128,16 +129,18 @@ class PhysFieldsBase3D(PhysFieldsBase2D):
         assert key_field is not None
 
         if equation.startswith("iz=") or equation.startswith("z="):
-            vecx = vector + "x"
-            vecy = vector + "y"
+            xlabel = "x"
+            ylabel = "y"
         elif equation.startswith("iy=") or equation.startswith("y="):
-            vecx = vector + "x"
-            vecy = vector + "z"
+            xlabel = "x"
+            ylabel = "z"
         elif equation.startswith("ix=") or equation.startswith("x="):
-            vecx = vector + "y"
-            vecy = vector + "z"
+            xlabel = "y"
+            ylabel = "z"
         else:
             raise NotImplementedError
+        vecx = vector + xlabel
+        vecy = vector + ylabel
 
         keys_state_phys = self.sim.state.keys_state_phys
         if vecx not in keys_state_phys or vecy not in keys_state_phys:
@@ -241,9 +244,207 @@ class PhysFieldsBase3D(PhysFieldsBase2D):
             vmax = None
 
         if mpi.rank == 0:
-            ax.set_xlabel("x")
-            ax.set_ylabel("y")
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
             self._set_title(ax, key_field, time, vmax)
+
+            if SCALED:
+                ax.axis("scaled")
+
+            fig.tight_layout()
+            fig.canvas.draw()
+            plt.pause(1e-3)
+
+    def plot_mean(
+        self,
+        field=None,
+        tmin=None,
+        tmax=None,
+        QUIVER=True,
+        vector="v",
+        equation=None,
+        nb_contours=20,
+        type_plot="contourf",
+        vmin=None,
+        vmax=None,
+        cmap="viridis",
+        numfig=None,
+        SCALED=True,
+    ):
+        """Plot the time average of a field.
+
+        Parameters
+        ----------
+
+        field : str, optional
+
+        tmin : number, optional
+
+        tmax : number, optional
+
+        QUIVER : True
+
+        vecx : 'ux'
+
+        vecy : 'uy'
+
+        nb_contours : 20
+
+        type_plot : 'contourf'
+
+        vmin : None
+
+        vmax : None
+
+        cmap : 'viridis'
+
+        numfig : None
+
+        SCALED : True
+
+        """
+
+        if equation is not None:
+            self.set_equation_crosssection(equation)
+        equation = self._equation
+
+        self._has_uxuy = self.sim.state.has_vars("vx", "vy")
+
+        key_field = None
+        if field is None:
+            key_field = self.get_key_field_to_plot(forbid_compute=True)
+        elif isinstance(field, str):
+            key_field = field
+
+        assert key_field is not None
+
+        if equation.startswith("iz=") or equation.startswith("z="):
+            xlabel = "x"
+            ylabel = "y"
+        elif equation.startswith("iy=") or equation.startswith("y="):
+            xlabel = "x"
+            ylabel = "z"
+        elif equation.startswith("ix=") or equation.startswith("x="):
+            xlabel = "y"
+            ylabel = "z"
+        else:
+            raise NotImplementedError
+        vecx = vector + xlabel
+        vecy = vector + ylabel
+
+        keys_state_phys = self.sim.state.keys_state_phys
+        if vecx not in keys_state_phys or vecy not in keys_state_phys:
+            QUIVER = False
+
+        self.set_of_phys_files.update_times()
+        times = self.set_of_phys_files.times
+        if tmin is None:
+            # get tmin from times
+            tmin = times.min()
+
+        if tmax is None:
+            # get tmax from times
+            tmax = times.max()
+
+        # we have to get the field from a file
+        if key_field not in self.sim.state.keys_state_phys:
+            error_message = (
+                f'key "{key_field}" not in state.keys_state_phys '
+                f"({self.sim.state.keys_state_phys})."
+            )
+            raise ValueError(error_message)
+
+        # get times for average
+        times_avg = times[(times >= tmin) & (times <= tmax)]
+
+        # initialize
+        time = times_avg[0]
+        field_onetime, time = self.get_field_to_plot(
+            key=key_field, time=time, equation=equation
+        )
+        field_avg = np.zeros_like(field_onetime)
+        vecx_avg = np.zeros_like(field_onetime)
+        vecy_avg = np.zeros_like(field_onetime)
+        # time average
+        for time in times_avg:
+            field_onetime, time = self.get_field_to_plot(
+                key=key_field, time=time, equation=equation
+            )
+            field_avg += field_onetime
+            if QUIVER:
+                field_onetime, time = self.get_field_to_plot(
+                    key=vecx, time=time, equation=equation
+                )
+                vecx_avg += field_onetime
+                field_onetime, time = self.get_field_to_plot(
+                    key=vecy, time=time, equation=equation
+                )
+                vecy_avg += field_onetime
+        field_avg /= times_avg.size
+        vecx_avg /= times_avg.size
+        vecy_avg /= times_avg.size
+
+        # plot
+        if mpi.rank == 0:
+            if numfig is None:
+                fig, ax = self.output.figure_axe()
+            else:
+                fig, ax = self.output.figure_axe(numfig=numfig)
+
+            x_seq, y_seq = self._get_grid1d(equation)
+
+            [XX_seq, YY_seq] = np.meshgrid(x_seq, y_seq)
+            try:
+                cmap = plt.get_cmap(cmap)
+            except ValueError:
+                print(
+                    "Use matplotlib >= 1.5.0 for new standard colorschemes.\
+                       Installed matplotlib :"
+                    + plt.matplotlib.__version__
+                )
+                cmap = plt.get_cmap("jet")
+
+            if type_plot == "contourf":
+                contours = ax.contourf(
+                    x_seq,
+                    y_seq,
+                    field_avg,
+                    nb_contours,
+                    vmin=vmin,
+                    vmax=vmax,
+                    cmap=cmap,
+                )
+                fig.colorbar(contours)
+                fig.contours = contours
+            elif type_plot == "pcolor":
+                pc = ax.pcolormesh(
+                    x_seq,
+                    y_seq,
+                    field_avg,
+                    shading="nearest",
+                    vmin=vmin,
+                    vmax=vmax,
+                    cmap=cmap,
+                )
+                fig.colorbar(pc)
+        else:
+            ax = None
+
+        if QUIVER:
+            quiver, vmax = self._quiver_plot(ax, vecx_avg, vecy_avg)
+        else:
+            vmax = None
+
+        if mpi.rank == 0:
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
+            title = key_field + f" $(tmin={tmin:8.6g}, tmax={tmax:8.6g})$"
+            if vmax is not None:
+                title += r", $|\vec{v}|_{max} = $" + f"{vmax:.3f}"
+            ax.set_title(title + "\n" + self.output.summary_simul)
+
+            if SCALED:
+                ax.axis("scaled")
 
             fig.tight_layout()
             fig.canvas.draw()
