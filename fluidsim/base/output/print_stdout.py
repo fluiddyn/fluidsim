d4cc0d0c5ac6e20615847c754cb8054aab0a0e03
paugier <pierre.augier@univ-grenoble-alpes.fr>
2020-12-14 16:15:28 +0100

    plot_clock_times: add last remaining_clock_times as str

--- fluidsim/base/output/print_stdout.py
+++ fluidsim/base/output/print_stdout.py
@@ -231,9 +231,23 @@ class PrintStdOutBase:
 
         fig, axes = plt.subplots(2, 1, sharex=True)
 
+        remaining_clock_times = results["remaining_clock_times"]
+
         ax = axes[0]
-        ax.plot(equation_times, results["remaining_clock_times"])
+        ax.plot(equation_times, remaining_clock_times)
         ax.set_ylabel("estimated full clock time (s)")
+
+        if remaining_clock_times[-1] > 0.05 * remaining_clock_times.max():
+            last_remaining = remaining_clock_times[-1]
+            ax.plot(
+                equation_times[-1],
+                last_remaining,
+                "rx",
+                label=(
+                    f"Last estimation {timedelta(seconds=int(last_remaining))}"
+                ),
+            )
+            ax.legend()
 
         clock_times_per_timestep = results["clock_times_per_timestep"]
         if clock_times_per_timestep[-1] <= 0.0:
@@ -254,7 +268,9 @@ class PrintStdOutBase:
         ax.set_xlabel("equation time")
         ax.set_ylabel("clock time per time step (s)")
         full_clock_time = results["full_clock_time"]
-        ax.set_title(f"Full clock time: {timedelta(seconds=full_clock_time)}")
+        ax.set_title(
+            f"Full clock time: {timedelta(seconds=int(full_clock_time))}"
+        )
 
         for ax in axes:
             ax.set_ylim(bottom=0)
