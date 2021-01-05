e5240192f08d47808be0e1423a473586da786f18
Ashwin Vishnu <avmo@kth.se>
2021-01-05 16:58:29 +0100

    Move IPython magics to MagicsCore

--- fluidsim/magic.py
+++ fluidsim/magic.py
@@ -6,27 +6,21 @@
    :private-members:
 
 """
-
-from IPython.core.magic import Magics, magics_class, line_magic
-from IPython.core import magic_arguments
-
-from pprint import pprint
-
 from fluiddyn.io.query import query_yes_no
-
-from fluidsim.util.util import (
-    available_solver_keys,
-    import_simul_class_from_key,
-    load_sim_for_plot,
-    load_state_phys_file,
+from fluidsim_core.magic import MagicsCore
+from IPython.core.magic import line_magic, magics_class
+from IPython.core.magic_arguments import (
+    argument,
+    magic_arguments,
+    parse_argstring,
 )
+
+from fluidsim.util.util import load_sim_for_plot, load_state_phys_file
 
 
 @magics_class
-class FluidsimMagics(Magics):
-    """Magics simplifies the instantiation steps for a Simul object.
-
-    It can be loaded in IPython or Jupyter as
+class FluidsimMagics(MagicsCore):
+    """Magic commands can be loaded in IPython or Jupyter as
 
     >>> %load_ext fluidsim.magic
 
@@ -76,63 +70,14 @@ class FluidsimMagics(Magics):
 
     """
 
-    def __init__(self, shell, package_solvers):
-        super().__init__(shell)
-        self.package = package_solvers
+    entrypoint_grp = "fluidsim.solvers"
 
-    def is_defined(self, varname):
-        user_ns = self.shell.user_ns
-        return varname in user_ns and user_ns[varname] is not None
-
-    @magic_arguments.magic_arguments()
-    @magic_arguments.argument("solver", nargs="?", default="")
-    @magic_arguments.argument("-f", "--force-overwrite", action="store_true")
-    @line_magic
-    def fluidsim(self, line):
-        args = magic_arguments.parse_argstring(self.fluidsim, line)
-
-        if args.solver == "":
-            print("Available solvers:")
-            pprint(available_solver_keys(self.package), indent=4, compact=True)
-
-            if self.is_defined("params") and self.is_defined("sim"):
-                print("\nInitialized:")
-                user_ns = self.shell.user_ns
-                pprint(
-                    dict(
-                        params=type(user_ns["params"]), Simul=type(user_ns["sim"])
-                    ),
-                    indent=4,
-                )
-            return
-
-        if not args.force_overwrite and (
-            self.is_defined("params") or self.is_defined("Simul")
-        ):
-            if not query_yes_no(
-                "At least one of the variables `params` or `Simul` is defined"
-                " in your user namespace. Do you really want to overwrite "
-                "them?"
-            ):
-                return
-
-        user_ns = self.shell.user_ns
-        Simul = import_simul_class_from_key(args.solver, self.package)
-        params = Simul.create_default_params()
-        print(
-            "Created Simul class and default parameters for",
-            line,
-            "-> Simul, params",
-        )
-        user_ns["Simul"] = Simul
-        user_ns["params"] = params
-
-    @magic_arguments.magic_arguments()
-    @magic_arguments.argument("-f", "--force-overwrite", action="store_true")
-    @magic_arguments.argument("-s", "--state-phys", action="store_true")
-    @magic_arguments.argument("-t", "--t-approx", type=float, default=None)
-    @magic_arguments.argument("-m", "--merge-missing-params", action="store_true")
-    @magic_arguments.argument(
+    @magic_arguments()
+    @argument("-f", "--force-overwrite", action="store_true")
+    @argument("-s", "--state-phys", action="store_true")
+    @argument("-t", "--t-approx", type=float, default=None)
+    @argument("-m", "--merge-missing-params", action="store_true")
+    @argument(
         "directory",
         nargs="?",
         help="Optional: absolute path/relative path/name of directory",
@@ -140,7 +85,7 @@ class FluidsimMagics(Magics):
     )
     @line_magic
     def fluidsim_load(self, line):
-        args = magic_arguments.parse_argstring(self.fluidsim_load, line)
+        args = parse_argstring(self.fluidsim_load, line)
 
         if not args.force_overwrite and (self.is_defined("sim")):
             if not query_yes_no(
@@ -166,19 +111,8 @@ class FluidsimMagics(Magics):
         user_ns["params"] = sim.params
         user_ns["sim"] = sim
 
-    @magic_arguments.magic_arguments()
-    @line_magic
-    def fluidsim_reset(self, line):
-        for varname in ("sim", "params", "Simul"):
-            if self.is_defined(varname):
-                del self.shell.user_ns[varname]
-
-    @line_magic
-    def fluidsim_help(self, line):
-        print(4 * " " + self.__doc__)
-
 
 def load_ipython_extension(ipython):
     """Load the extension in IPython."""
-    fluidsim_magics = FluidsimMagics(ipython, "fluidsim.solvers")
+    fluidsim_magics = FluidsimMagics(ipython)
     ipython.register_magics(fluidsim_magics)
