cefb75fcae534eb917134451ac0118582670498b
paugier <pierre.augier@univ-grenoble-alpes.fr>
2021-02-17 16:19:29 +0100

    Fix bug with pytest and solvers/ad1d/pseudo_spect/test_solver.py

--- fluidsim/solvers/ad1d/pseudo_spect/test_solver.py
+++ fluidsim/solvers/ad1d/pseudo_spect/test_solver.py
@@ -1,9 +1,12 @@
 from .solver import Simul
 
-from fluidsim.solvers.ad1d.test_solver import TestSolverAD1D as Base
+from fluidsim.solvers.ad1d.test_solver import TestSolverAD1D
 from fluidsim.util.testing import skip_if_no_fluidfft
 
 
 @skip_if_no_fluidfft
-class TestSolverAD1DPseudoSpectral(Base):
+class TestSolverAD1DPseudoSpectral(TestSolverAD1D):
     Simul = Simul
+
+
+del TestSolverAD1D
