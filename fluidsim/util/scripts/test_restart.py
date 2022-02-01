34f9c821946011220ede6812517f2c7e9da8bb76
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-02-01 16:48:14 +0100

    scripts/test_restart: 100% coverage

--- fluidsim/util/scripts/test_restart.py
+++ fluidsim/util/scripts/test_restart.py
@@ -1,7 +1,6 @@
-
 import shutil
 import sys
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 from pathlib import Path
 
 import pytest
@@ -10,7 +9,8 @@ from fluiddyn.util import mpi
 
 from fluidsim.solvers.ns2d.solver import Simul
 
-from fluidsim.util.scripts.restart import main
+from fluidsim.util.scripts.restart import main, restart
+
 
 @pytest.fixture(scope="module")
 def path_simul():
@@ -24,7 +24,8 @@ def path_simul():
     params.init_fields.noise.length = 1.0
     params.init_fields.noise.velo_max = 1.0
 
-    params.time_stepping.t_end = 2 * params.time_stepping.deltat_max
+    params.time_stepping.t_end = 1.5 * params.time_stepping.deltat_max
+    params.time_stepping.it_end = 2
 
     sim = Simul(params)
 
@@ -37,18 +38,42 @@ def path_simul():
 
 
 def test_only_check(path_simul):
-    with patch.object(sys, "argv", ["fluidsim-restart", path_simul, "-oc"]):
+    argv = ["fluidsim-restart", path_simul, "-oc", "--it_end", "1"]
+    argv.extend(["--add-to-it_end", "1"])
+    with patch.object(sys, "argv", argv):
         main()
+
+
+def test_check_t_end(path_simul):
+    path_simul = Path(path_simul)
+    path_last_time = str(sorted(path_simul.glob("state_phys*"))[-1])
+    argv = [path_last_time, "-oi"]
+    restart(argv, add_to_it_end=2)
 
 
 def test_only_init(path_simul):
     path_simul = Path(path_simul)
-    print(sorted(path_simul.glob("*")))
     path_last_time = str(sorted(path_simul.glob("state_phys*"))[-1])
-    with patch.object(sys, "argv", ["fluidsim-restart", path_last_time, "-oi"]):
+    argv = ["fluidsim-restart", path_last_time, "-oi", "--t_end", "1.0"]
+    with patch.object(sys, "argv", argv):
+        main()
+
+
+def test_check_it_end(path_simul):
+    path_simul = Path(path_simul)
+    path_last_time = str(sorted(path_simul.glob("state_phys*"))[-1])
+    argv = [
+        "fluidsim-restart",
+        path_last_time,
+        "--modify-params",
+        "params.time_stepping.USE_T_END = False",
+    ]
+    with patch.object(sys, "argv", argv):
         main()
 
 
 def test_simul(path_simul):
-    with patch.object(sys, "argv", ["fluidsim-restart", path_simul, "--t_end", "1.0"]):
+    argv = ["fluidsim-restart", path_simul, "--t_end", "1.0"]
+    argv.extend(["--add-to-t_end", "1.0"])
+    with patch.object(sys, "argv", argv):
         main()
