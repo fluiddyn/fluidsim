c882890d7b3774a57aad101ecd3822b3f03cb23a
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-02-21 10:51:19 +0100

    Fix fluidsim/operators/test/test_operators3d.py::test_coarse_functions

--- fluidsim/operators/test/test_operators3d.py
+++ fluidsim/operators/test/test_operators3d.py
@@ -204,18 +204,18 @@ def test_divh_rotz(oper):
 def test_coarse_functions(oper):
     # A given random field
     f_fft = oper.create_arrayK_random()
-    nk0, nk1, nk2 = oper.Kx.shape   
+    nk0, nk1, nk2 = oper.Kx.shape
 
-    # We create coarsed field(s) from f_fft
-    nk0c, nk1c, nk2c = int(nk0/4), int(nk1), int(nk2)
+    # We create coarse field(s) from f_fft
+    nk0c, nk1c, nk2c = nk0 // 4, nk1 // 2, nk2
     fc_fft = oper.coarse_seq_from_fft_loc(f_fft, (nk0c, nk1c, nk2c))
 
-    # The initial field is compared to the field recomposed with the coarsed one(s)
-    f_fft_bis = oper.create_arrayK(value=0.)
-    oper.put_coarse_array_in_array_fft(fc_fft, f_fft_bis, oper, (nk0c, nk1c, nk2c))
+    f_fft_bis = oper.create_arrayK(value=0.0)
+    oper.put_coarse_array_in_array_fft(
+        fc_fft, f_fft_bis, oper, (nk0c, nk1c, nk2c)
+    )
+    fc_fft_bis = oper.coarse_seq_from_fft_loc(f_fft_bis, (nk0c, nk1c, nk2c))
 
-    print(fc_fft) 
-    print(f_fft.shape)
-    print(f_fft - f_fft_bis)  # TODO: Remove this line before merging  
-    assert np.sum(np.abs(f_fft - f_fft_bis)) == 0.0, "Errors in coarse functions"
-
+    assert np.allclose(fc_fft, fc_fft_bis)
+    # check that the coarse arrays are not full of zeros
+    assert (abs(fc_fft_bis) ** 2).mean() > 0.1
