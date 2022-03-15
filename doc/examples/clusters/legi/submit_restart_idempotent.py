45d94e2b0de741a31460ce37d7c735048e6af482
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-03-15 09:49:56 +0100

    Improve forcing_anisotropic_3d/submit_toro.py

--- doc/examples/clusters/legi/submit_restart_idempotent.py
+++ doc/examples/clusters/legi/submit_restart_idempotent.py
@@ -1,4 +1,4 @@
-from fluiddyn.clusters.legi import Calcul8 as C
+from fluiddyn.clusters.legi import Calcul2 as C
 
 cluster = C()
 
@@ -11,7 +11,7 @@ cluster.commands_setting_env = [
 
 cluster.submit_command(
     "fluidsim-restart . --t_end 30",
-    name_run="fluiddyn",
+    name_run="fluidsim-restart",
     nb_nodes=1,
     walltime="04:00:00",
     nb_mpi_processes=cluster.nb_cores_per_node,
--- doc/examples/forcing_anisotropic_3d/submit_toro.py
+++ doc/examples/forcing_anisotropic_3d/submit_toro.py
@@ -9,14 +9,32 @@ cluster.commands_setting_env = [
     "export FLUIDSIM_PATH=/fsnet/project/meige/2022/22STRATURBANIS",
 ]
 
+N = 40
+R = 5
+
+assert N in [10, 20, 40]
+assert R in [5, 10, 20, 40, 80, 160]
+
+nh = 320
+if N == 40:
+    ratio_nh_nz = 8
+elif N == 20:
+    ratio_nh_nz = 4
+elif N == 10:
+    ratio_nh_nz = 2
+else:
+    raise NotImplementedError
+
+nz = nh // ratio_nh_nz
+
 cluster.submit_command(
     (
-        "./run_simul_toro.py -nz 80 -N 20 -R 40"
+        f"./run_simul_toro.py -R {R} -N {N} --ratio-nh-nz {ratio_nh_nz} -nz {nz}"
     ),
     name_run="fluiddyn",
     nb_nodes=1,
-    walltime="24:00:00",
-    nb_mpi_processes=cluster.nb_cores_per_node,
+    walltime="10:00:00",
+    nb_mpi_processes=4,  # cluster.nb_cores_per_node // 2,
     omp_num_threads=1,
     delay_signal_walltime=300,
     ask=True,
