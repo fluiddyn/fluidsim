4e7ba696bcc140c3392d70540fa5e5683ef120e8
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-03-17 16:20:58 +0100

    Only handle signals with rank 0

--- fluidsim/base/time_stepping/base.py
+++ fluidsim/base/time_stepping/base.py
@@ -102,15 +102,17 @@ max_elapsed: number or str (default None)
 
         self._stop_signal_received = False
 
-        def handler_signals(signal_number, stack):
-            print(f"signal {signal_number} received (rank {mpi.rank}).")
-            self._stop_signal_received = True
+        if mpi.rank == 0:
 
-        try:
-            # 12 is SIGUSR2 (warning: not propagated by MPICH)
-            signal(12, handler_signals)
-        except ValueError:
-            warn("Cannot handle signals - is multithreading on?")
+            def handler_signals(signal_number, stack):
+                print(f"signal {signal_number} received (rank {mpi.rank}).")
+                self._stop_signal_received = True
+
+            try:
+                # 12 is SIGUSR2 (warning: not propagated by MPICH)
+                signal(12, handler_signals)
+            except ValueError:
+                warn("Cannot handle signals - is multithreading on?")
 
         self._has_to_stop = False
 
