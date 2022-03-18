bee7adc06e74ea2f4a1872dc49693e7381c92d42
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-03-18 15:06:51 +0100

    itertools.product in examples/forcing_anisotropic_3d/submit_toro*

--- doc/examples/forcing_anisotropic_3d/submit_toro320.py
+++ doc/examples/forcing_anisotropic_3d/submit_toro320.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from time import sleep
+from itertools import product
 
 from fluiddyn.clusters.legi import Calcul8 as C
 from fluiddyn.clusters.oar import get_job_id
@@ -39,79 +40,76 @@ def get_ratio_nh_nz(N):
         raise NotImplementedError
 
 
-for N in [10, 20, 40]:
-    for Rb in [5, 10, 20, 40, 80, 160]:
-        if N == 40 and Rb == 160:
-            continue
+for N, Rb in product([10, 20, 40], [5, 10, 20, 40, 80, 160]):
+    if N == 40 and Rb == 160:
+        continue
 
-        ratio_nh_nz = get_ratio_nh_nz(N)
-        nz = nh // ratio_nh_nz
+    ratio_nh_nz = get_ratio_nh_nz(N)
+    nz = nh // ratio_nh_nz
 
-        name_1st_run = f"run_simul_toro_nx{nh}_Rb{Rb}_N{N}"
-        job_id = get_job_id(name_1st_run)
-        try:
-            path = [
-                p for p in paths if f"_Rb{Rb}_" in p.name and f"_N{N}_" in p.name
-            ][0]
-        except IndexError:
-            if job_id is None:
-                command = f"./run_simul_toro.py -R {Rb} -N {N} --ratio-nh-nz {ratio_nh_nz} -nz {nz} --t_end {t_end}"
-
-                cluster.submit_command(
-                    command,
-                    name_run=name_1st_run,
-                    nb_nodes=1,
-                    walltime=walltime,
-                    nb_mpi_processes=10,
-                    omp_num_threads=1,
-                    delay_signal_walltime=300,
-                    ask=False,
-                )
-
-                while job_id is None:
-                    job_id = get_job_id(name_1st_run)
-                    sleep(1)
-            else:
-                print(
-                    f"Nothing to do for nx{nh}_Rb{Rb}_N{N} because first job is "
-                    "already launched and the simulation directory is not created"
-                )
-                continue
-
-        else:
-
-            t_start, t_last = times_start_last_from_path(path)
-            if t_last > t_end:
-                print(f"Nothing to do for {path.name} because t_last > t_end")
-                continue
-
-            try:
-                estimated_remaining_duration = (
-                    get_last_estimated_remaining_duration(path)
-                )
-            except RuntimeError:
-                estimated_remaining_duration = "?"
-
-            print(f"{path.name}: {t_last = }, {estimated_remaining_duration = }")
-
-            command = f"fluidsim-restart {path}"
-            name_run = command.split()[0] + f"_nx{nh}_Rb{Rb}_N{N}"
-
-            if get_job_id(name_run) is not None:
-                print(
-                    f"Nothing to do because the idempotent job is already launched"
-                )
-                continue
+    name_1st_run = f"run_simul_toro_nx{nh}_Rb{Rb}_N{N}"
+    job_id = get_job_id(name_1st_run)
+    try:
+        path = [
+            p for p in paths if f"_Rb{Rb}_" in p.name and f"_N{N}_" in p.name
+        ][0]
+    except IndexError:
+        if job_id is None:
+            command = f"./run_simul_toro.py -R {Rb} -N {N} --ratio-nh-nz {ratio_nh_nz} -nz {nz} --t_end {t_end}"
 
             cluster.submit_command(
                 command,
-                name_run=name_run,
+                name_run=name_1st_run,
                 nb_nodes=1,
                 walltime=walltime,
                 nb_mpi_processes=10,
                 omp_num_threads=1,
                 delay_signal_walltime=300,
                 ask=False,
-                idempotent=True,
-                anterior=job_id,
             )
+
+            while job_id is None:
+                job_id = get_job_id(name_1st_run)
+                sleep(1)
+        else:
+            print(
+                f"Nothing to do for nx{nh}_Rb{Rb}_N{N} because first job is "
+                "already launched and the simulation directory is not created"
+            )
+            continue
+
+    else:
+
+        t_start, t_last = times_start_last_from_path(path)
+        if t_last > t_end:
+            print(f"Nothing to do for {path.name} because t_last > t_end")
+            continue
+
+        try:
+            estimated_remaining_duration = get_last_estimated_remaining_duration(
+                path
+            )
+        except RuntimeError:
+            estimated_remaining_duration = "?"
+
+        print(f"{path.name}: {t_last = }, {estimated_remaining_duration = }")
+
+        command = f"fluidsim-restart {path}"
+        name_run = command.split()[0] + f"_nx{nh}_Rb{Rb}_N{N}"
+
+        if get_job_id(name_run) is not None:
+            print(f"Nothing to do because the idempotent job is already launched")
+            continue
+
+        cluster.submit_command(
+            command,
+            name_run=name_run,
+            nb_nodes=1,
+            walltime=walltime,
+            nb_mpi_processes=10,
+            omp_num_threads=1,
+            delay_signal_walltime=300,
+            ask=False,
+            idempotent=True,
+            anterior=job_id,
+        )
--- doc/examples/forcing_anisotropic_3d/submit_toro_big.py
+++ doc/examples/forcing_anisotropic_3d/submit_toro_big.py
@@ -12,6 +12,7 @@ python submit_toro_big.py
 import subprocess
 from pathlib import Path
 from time import sleep
+from itertools import product
 
 from fluiddyn.clusters.legi import Calcul8 as C
 from fluiddyn.clusters.oar import get_job_id
@@ -49,120 +50,117 @@ def filter_path(paths, Rb, N):
     return [p for p in paths if f"_Rb{Rb}_" in p.name and f"_N{N}_" in p.name][0]
 
 
-for N in [10, 20, 40]:
-    for Rb in [5, 10, 20, 40, 80, 160]:
-        if N == 40 and Rb == 160:
-            continue
+for N, Rb in product([10, 20, 40], [5, 10, 20, 40, 80, 160]):
+    if N == 40 and Rb == 160:
+        continue
 
-        if Rb == 5 and N == 10:
-            continue
+    if Rb == 5 and N == 10:
+        continue
 
-        name_1st_run = f"from_modified_resol_nx{nh}_Rb{Rb}_N{N}"
-        job_id = get_job_id(name_1st_run)
-        try:
-            path = filter_path(paths, Rb, N)
-        except IndexError:
-            if job_id is None:
+    name_1st_run = f"from_modified_resol_nx{nh}_Rb{Rb}_N{N}"
+    job_id = get_job_id(name_1st_run)
+    try:
+        path = filter_path(paths, Rb, N)
+    except IndexError:
+        if job_id is None:
 
-                try:
-                    path_init = filter_path(paths_init, Rb, N)
-                except IndexError:
-                    print(
-                        f"Cannot do anything for nx{nh}_Rb{Rb}_N{N} because no init directory"
-                    )
-                    continue
-
-                t_start, t_last = times_start_last_from_path(path_init)
-                if t_last < t_init:
-                    try:
-                        estimated_remaining_duration = (
-                            get_last_estimated_remaining_duration(path_init)
-                        )
-                    except RuntimeError:
-                        estimated_remaining_duration = "?"
-
-                    print(
-                        f"Cannot launch {name_1st_run} because the coarse "
-                        "simulation is not finished "
-                        f"({t_last=} < {t_init=}, {estimated_remaining_duration = })"
-                    )
-                    continue
-
-                try:
-                    path_init_file = next(
-                        path_init.glob(f"State_phys_{nh}x{nh}*/state_phys_t*.h5")
-                    )
-                except StopIteration:
-                    subprocess.run(
-                        ["fluidsim-modif-resolution", str(path_init), "2"],
-                        check=True,
-                    )
-                    path_init_file = next(
-                        path_init.glob(f"State_phys_{nh}x{nh}*/state_phys_t*.h5")
-                    )
-
-                command = (
-                    f"fluidsim-restart {path_init_file} --t_end {t_end} --new-dir-results "
-                    "--modify-params 'params.nu_4 /= 10; params.output.periods_save.phys_fields = 0.5; "
-                    "params.output.periods_save.spatiotemporal_spectra = 2 * pi / (params.N * 4)'"
-                )
-
-                cluster.submit_command(
-                    command,
-                    name_run=name_1st_run,
-                    nb_nodes=1,
-                    walltime="04:00:00",
-                    nb_mpi_processes=10,
-                    omp_num_threads=1,
-                    delay_signal_walltime=300,
-                    ask=False,
-                )
-
-                while job_id is None:
-                    job_id = get_job_id(name_1st_run)
-                    sleep(0.2)
-            else:
+            try:
+                path_init = filter_path(paths_init, Rb, N)
+            except IndexError:
                 print(
-                    f"Nothing to do for nx{nh}_Rb{Rb}_N{N} because first job is "
-                    "already launched and the simulation directory is not created"
+                    f"Cannot do anything for nx{nh}_Rb{Rb}_N{N} because no init directory"
                 )
                 continue
 
-        else:
-            command = f"fluidsim-restart {path}"
-            name_run = command.split()[0] + f"_nx{nh}_Rb{Rb}_N{N}"
+            t_start, t_last = times_start_last_from_path(path_init)
+            if t_last < t_init:
+                try:
+                    estimated_remaining_duration = (
+                        get_last_estimated_remaining_duration(path_init)
+                    )
+                except RuntimeError:
+                    estimated_remaining_duration = "?"
 
-            t_start, t_last = times_start_last_from_path(path)
-            if t_last > t_end:
                 print(
-                    f"Cannot launch {name_run} for {path.name} because {t_last=} > {t_end=}"
+                    f"Cannot launch {name_1st_run} because the coarse "
+                    "simulation is not finished\n"
+                    f"  ({t_last=} < {t_init=}, {estimated_remaining_duration = })"
                 )
                 continue
 
             try:
-                estimated_remaining_duration = (
-                    get_last_estimated_remaining_duration(path)
+                path_init_file = next(
+                    path_init.glob(f"State_phys_{nh}x{nh}*/state_phys_t*.h5")
                 )
-            except RuntimeError:
-                estimated_remaining_duration = "?"
-
-            print(f"{path.name}: {t_last = }, {estimated_remaining_duration = }")
-
-            if get_job_id(name_run) is not None:
-                print(
-                    "Nothing to do because the idempotent job is already launched"
+            except StopIteration:
+                subprocess.run(
+                    ["fluidsim-modif-resolution", str(path_init), "2"],
+                    check=True,
                 )
-                continue
+                path_init_file = next(
+                    path_init.glob(f"State_phys_{nh}x{nh}*/state_phys_t*.h5")
+                )
+
+            command = (
+                f"fluidsim-restart {path_init_file} --t_end {t_end} --new-dir-results "
+                "--modify-params 'params.nu_4 /= 10; params.output.periods_save.phys_fields = 0.5; "
+                "params.output.periods_save.spatiotemporal_spectra = 2 * pi / (params.N * 4)'"
+            )
 
             cluster.submit_command(
                 command,
-                name_run=name_run,
+                name_run=name_1st_run,
                 nb_nodes=1,
                 walltime="04:00:00",
                 nb_mpi_processes=10,
                 omp_num_threads=1,
                 delay_signal_walltime=300,
                 ask=False,
-                idempotent=True,
-                anterior=job_id,
             )
+
+            while job_id is None:
+                job_id = get_job_id(name_1st_run)
+                sleep(0.2)
+        else:
+            print(
+                f"Nothing to do for nx{nh}_Rb{Rb}_N{N} because first job is "
+                "already launched and the simulation directory is not created"
+            )
+            continue
+
+    else:
+        command = f"fluidsim-restart {path}"
+        name_run = command.split()[0] + f"_nx{nh}_Rb{Rb}_N{N}"
+
+        t_start, t_last = times_start_last_from_path(path)
+        if t_last > t_end:
+            print(
+                f"Simulation {path.name} done! {t_last=} > {t_end=}"
+            )
+            continue
+
+        try:
+            estimated_remaining_duration = get_last_estimated_remaining_duration(
+                path
+            )
+        except RuntimeError:
+            estimated_remaining_duration = "?"
+
+        print(f"{path.name}: {t_last = }, {estimated_remaining_duration = }")
+
+        if get_job_id(name_run) is not None:
+            print("  Nothing to do because the idempotent job is already launched")
+            continue
+
+        cluster.submit_command(
+            command,
+            name_run=name_run,
+            nb_nodes=1,
+            walltime="08:00:00",
+            nb_mpi_processes=10,
+            omp_num_threads=1,
+            delay_signal_walltime=300,
+            ask=False,
+            idempotent=True,
+            anterior=job_id,
+        )
