493b22f1861c40348f8ea2a58a7fe0023405d9ad
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-04-07 14:27:22 +0200

    forcing_anisotropic_3d/toro2022: improve analyse_dimensionless.ipynb

--- doc/examples/forcing_anisotropic_3d/toro2022/occigen/rsync_end_states.sh
+++ doc/examples/forcing_anisotropic_3d/toro2022/occigen/rsync_end_states.sh
@@ -3,3 +3,5 @@
 rsync -rvz -L \
   augier@occigen.cines.fr:/scratch/cnt0022/egi2153/augier/aniso/end_states \
   /fsnet/project/meige/2022/22STRATURBANIS/from_occigen
+
+du -h /fsnet/project/meige/2022/22STRATURBANIS/from_occigen/end_states
