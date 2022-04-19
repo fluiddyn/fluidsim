72b3e519c10c01650aa7244a19a8469e88f86f87
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-04-19 14:26:12 +0200

    forcing_anisotropic_3d/toro2022: rsync spatiotemp data for N=80 and 120

--- doc/examples/forcing_anisotropic_3d/toro2022/occigen/rsync_small.sh
+++ doc/examples/forcing_anisotropic_3d/toro2022/occigen/rsync_small.sh
@@ -3,6 +3,8 @@
 rsync -rvz \
   augier@occigen.cines.fr:/scratch/cnt0022/egi2153/augier/aniso \
   /fsnet/project/meige/2022/22STRATURBANIS/from_occigen \
+  --include "aniso/ns3d.strat*_896x896x*_N120*/spatiotemporal/*" \
+  --include "aniso/ns3d.strat*_896x896x*_N80*/spatiotemporal/*" \
   --exclude "aniso/*/spatiotemporal/rank*_tmin*.h5" \
   --exclude "aniso/*/state_phys_t*.h5" \
   --exclude "aniso/*/State_phys_*/state_phys_t*.h5" \
