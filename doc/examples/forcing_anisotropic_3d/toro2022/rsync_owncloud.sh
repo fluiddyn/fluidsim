94c67b0ab4b7b3d1e2d991e0078f765bbfb09060
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-05-05 16:33:33 +0200

    forcing_anisotropic_3d/toro2022/rsync_owncloud.sh

---
+++ doc/examples/forcing_anisotropic_3d/toro2022/rsync_owncloud.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rsync -rv \
+  /fsnet/project/meige/2022/22STRATURBANIS/results_papermill/* \
+  /fsnet/project/meige/2022/22STRATURBANIS/from_occigen/aniso/results_papermill/* \
+  /fsnet/project/meige/2013/13MUST/MyCore/2022strat-turb-toro/notebooks
+
+rsync -rv \
+  /fsnet/project/meige/2022/22STRATURBANIS/aniso/ns3d* \
+  /fsnet/project/meige/2022/22STRATURBANIS/from_occigen/aniso/ns3d* \
+  /fsnet/project/meige/2013/13MUST/MyCore/2022strat-turb-toro/simul_folders \
+  --exclude "**/spatiotemporal/rank*_tmin*.h5" \
+  --exclude "**/state_phys_t*.h5" \
+  --exclude "end_states/*" \
+  --exclude "results_papermill/*"
