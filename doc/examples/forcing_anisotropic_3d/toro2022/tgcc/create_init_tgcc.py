9bb4847d0e9c7e81286f0c5739df0a096ffce3b5
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-06-13 16:00:40 +0200

    toro2022/tgcc: create_init_tgcc.py

---
+++ doc/examples/forcing_anisotropic_3d/toro2022/tgcc/create_init_tgcc.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from runpy import run_path
+
+import pandas as pd
+
+new_simuls = pd.read_csv("new_simulations20220613.csv")
+
+
+here = Path(__file__).absolute().parent
+
+util_main = run_path(here.parent / "util.py")
+path_base = util_main["path_base"]
+
+path_init_tgcc = path_base / "init_tgcc"
+
+path_end_state_occigen = path_base / "from_occigen/end_states"
+path_legi = path_base / "aniso"
+
+for name in new_simuls.init:
+
+    if name.startswith("("):
+        continue
+    nh = int(name.split("_", 1)[1].split("x", 1)[0])
+
+    if nh > 640:
+        path_base_input = path_end_state_occigen
+    else:
+        path_base_input = path_legi
+
+    path_input = sorted(path_base_input.glob(f"*{name}*/state_phys_*"))[-1]
+    print(path_input)
+
+    link_last_state = path_init_tgcc / path_input.parent.name / path_input.name
+
+    if not link_last_state.exists():
+        link_last_state.parent.mkdir(exist_ok=True)
+        link_last_state.symlink_to(path_input)
+        print(f"Link {link_last_state} created")
