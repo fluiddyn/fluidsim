b9d6cad386f2bab6e94376d8dd7be16bcc6e5d32
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-08-31 11:37:49 +0200

    Refactor to simplify reading

--- fluidsim/base/turb_model/__init__.py
+++ fluidsim/base/turb_model/__init__.py
@@ -28,7 +28,7 @@ Examples
 
 from fluidsim.extend_simul import extend_simul_class
 
-from .specific import SmagorinskyModel
+from .smagorinsky import SmagorinskyModel
 
 
 __all__ = ["extend_simul_class", "SmagorinskyModel"]
--- fluidsim/base/turb_model/smagorinsky.py
+++ fluidsim/base/turb_model/smagorinsky.py
@@ -1,15 +1,7 @@
-"""Specific turbulent models (:mod:`fluidsim.base.turb_model.specific`)
-=======================================================================
+"""Smagorinsky turbulent models (:mod:`fluidsim.base.turb_model.smagorinsky`)
+=============================================================================
 
 Provides:
-
-.. autoclass:: SpecificTurbModel
-   :members:
-   :private-members:
-
-.. autoclass:: SpecificTurbModelSpectral
-   :members:
-   :private-members:
 
 .. autoclass:: SmagorinskyModel
    :members:
@@ -19,51 +11,8 @@ Provides:
 
 from math import sqrt
 
-from fluidsim.extend_simul import SimulExtender
-from fluidsim.base.setofvariables import SetOfVariables
-
-from .stress_tensor import StressTensorComputer3D
-
-
-class SpecificTurbModel(SimulExtender):
-    _module_name = "fluidsim.base.turb_model.specific"
-    tag = "specific_turb_model"
-
-    @classmethod
-    def get_modif_info_solver(cls):
-        """Create a function to modify ``info_solver``.
-
-        Note that this function is called when the object ``info_solver`` has
-        not yet been created (and cannot yet be modified)! This is why one
-        needs to create a function that will be called later to modify
-        ``info_solver``.
-
-        """
-
-        def modif_info_solver(info_solver):
-
-            from fluidsim.base.turb_model.base import modif_infosolver_turb_model
-
-            modif_infosolver_turb_model(info_solver)
-
-            info_solver.classes.TurbModel.classes._set_child(
-                cls.tag,
-                attribs={
-                    "module_name": cls._module_name,
-                    "class_name": cls.__name__,
-                },
-            )
-
-        return modif_info_solver
-
-
-class SpecificTurbModelSpectral(SpecificTurbModel):
-    def __init__(self, sim):
-        self.sim = sim
-
-        self.forcing_fft = SetOfVariables(
-            like=sim.state.state_spect, info="forcing_fft", value=0.0
-        )
+from fluidsim.base.turb_model.base import SpecificTurbModelSpectral
+from fluidsim.base.turb_model.stress_tensor import StressTensorComputer3D
 
 
 class SmagorinskyModel(SpecificTurbModelSpectral):
@@ -87,6 +36,7 @@ class SmagorinskyModel(SpecificTurbModelSpectral):
     with :math:`C = 0.18` and :math:`\Delta = L_x / n_x`.
 
     """
+    _module_name = "fluidsim.base.turb_model.smagorinsky"
     tag = "smagorinsky"
 
     @classmethod
