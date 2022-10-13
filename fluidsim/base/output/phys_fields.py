a99cc7a2cddff51aa6a50efac82b69af28a77a66
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-10-13 14:25:16 +0200

    SetOfPhysFieldFiles in fluidsim_core/output/phys_fields.py

--- fluidsim/base/output/phys_fields.py
+++ fluidsim/base/output/phys_fields.py
@@ -17,14 +17,15 @@ Provides:
 
 import re
 import os
-from glob import glob
 from pathlib import Path
-from math import isclose
+import math
 
 import numpy as np
 import h5py
 
 from fluiddyn.util import mpi
+
+from fluidsim_core.output.phys_fields import SetOfPhysFieldFilesBase
 
 from fluidsim.util.output import save_file, h5pack, ext
 
@@ -106,7 +107,7 @@ class PhysFieldsBase(SpecificOutput):
         # first called by the super().__init__ function when set_of_phys_files
         # is not initialized (see above). Useful when end_of_simul is called.
         if hasattr(self, "set_of_phys_files"):
-            self.set_of_phys_files.path_dir = self.output.path_run
+            self.set_of_phys_files.path_dir = Path(self.output.path_run)
 
     def _init_files(self, arrays_1st_time=None):
         # Does nothing on purpose...
@@ -221,7 +222,7 @@ class PhysFieldsBase(SpecificOutput):
 
         if (
             not self.sim.params.ONLY_COARSE_OPER
-            and (time is None or isclose(time, self.sim.time_stepping.t))
+            and (time is None or math.isclose(time, self.sim.time_stepping.t))
             and idx_time is None
         ):
             # we get the field from the state
@@ -344,99 +345,10 @@ def time_from_path(path):
     return time
 
 
-class SetOfPhysFieldFiles:
+class SetOfPhysFieldFiles(SetOfPhysFieldFilesBase):
     """A set of physical field files."""
 
-    def __init__(self, path_dir=None, output=None):
-        self.output = output
-        if path_dir is None:
-            if output is None:
-                path_dir = Path.cwd()
-            else:
-                path_dir = Path(output.path_run)
-        self.path_dir = Path(path_dir)
-        self.update_times()
-
-    def update_times(self):
-        """Initialize the times by globing and analyzing the file names."""
-        path_files = sorted(self.path_dir.glob("state_phys*.[hn]*"))
-
-        if hasattr(self, "path_files") and len(self.path_files) == len(
-            path_files
-        ):
-            return
-
-        self.path_files = sorted(path_files)
-        self.times = np.array([time_from_path(path) for path in self.path_files])
-
-    def get_min_time(self):
-        if hasattr(self, "times"):
-            return self.times.min()
-        return 0.0
-
-    def get_max_time(self):
-        return self.times.max()
-
-    def get_field_to_plot(
-        self,
-        time=None,
-        idx_time=None,
-        key=None,
-        equation=None,
-        interpolate_time=True,
-    ):
-
-        if time is None and idx_time is None:
-            self.update_times()
-            time = self.times[-1]
-
-        # Assert files are available
-        if self.times.size == 0:
-            raise FileNotFoundError(
-                f"No phys files were detected in directory: {self.path_dir}"
-            )
-
-        if not interpolate_time and time is not None:
-            idx, time_closest = self.get_closest_time_file(time)
-            return self.get_field_to_plot(
-                idx_time=idx, key=key, equation=equation
-            )
-
-        if interpolate_time and time is not None:
-
-            idx_closest, time_closest = self.get_closest_time_file(time)
-
-            if isclose(time, time_closest) or self.times.size == 1:
-                return self.get_field_to_plot(
-                    idx_time=idx_closest, key=key, equation=equation
-                )
-
-            if idx_closest == self.times.size - 1:
-                idx0 = idx_closest - 1
-                idx1 = idx_closest
-            elif time_closest < time or idx_closest == 0:
-                idx0 = idx_closest
-                idx1 = idx_closest + 1
-            elif time_closest > time:
-                idx0 = idx_closest - 1
-                idx1 = idx_closest
-
-            dt_save = self.times[idx1] - self.times[idx0]
-            weight0 = 1 - np.abs(time - self.times[idx0]) / dt_save
-            weight1 = 1 - np.abs(time - self.times[idx1]) / dt_save
-
-            field0, time0 = self.get_field_to_plot(
-                idx_time=idx0, key=key, equation=equation
-            )
-            field1, time1 = self.get_field_to_plot(
-                idx_time=idx1, key=key, equation=equation
-            )
-
-            return field0 * weight0 + field1 * weight1, time
-
-        return self._get_field_to_plot_from_file(
-            self.path_files[idx_time], key, equation
-        )
+    time_from_path = staticmethod(time_from_path)
 
     def _get_field_to_plot_from_file(self, path_file, key, equation):
 
@@ -476,8 +388,3 @@ class SetOfPhysFieldFiles:
 
             else:
                 raise NotImplementedError
-
-    def get_closest_time_file(self, time):
-        """Find the index and value of the closest actual time of the field."""
-        idx = np.abs(self.times - time).argmin()
-        return idx, self.times[idx]
--- lib/fluidsim_core/output/phys_fields.py
+++ lib/fluidsim_core/output/phys_fields.py
@@ -15,9 +15,12 @@ These classes can be used to define classes working together with
 
 """
 
-from typing import Optional
-
 from abc import ABCMeta, abstractmethod
+from typing import Optional
+from pathlib import Path
+import math
+
+import numpy as np
 
 
 class SetOfPhysFieldFilesABC(metaclass=ABCMeta):
@@ -71,4 +74,113 @@ class PhysFieldsABC(metaclass=ABCMeta):
 
     @abstractmethod
     def _get_axis_data(equation=None):
+        ...
+
+
+class SetOfPhysFieldFilesBase(SetOfPhysFieldFilesABC):
+    @staticmethod
+    @abstractmethod
+    def time_from_path(path):
+        ...
+
+    def __init__(self, path_dir=None, output=None):
+        self.output = output
+        if path_dir is None:
+            if output is None:
+                path_dir = Path.cwd()
+            else:
+                path_dir = Path(output.path_run)
+        self.path_dir = Path(path_dir)
+        self.update_times()
+
+    def update_times(self):
+        """Initialize the times by globing and analyzing the file names."""
+        path_files = sorted(self.path_dir.glob("state_phys*.[hn]*"))
+
+        if hasattr(self, "path_files") and len(self.path_files) == len(
+            path_files
+        ):
+            return
+
+        self.path_files = sorted(path_files)
+        self.times = np.array(
+            [self.time_from_path(path) for path in self.path_files]
+        )
+
+    def get_min_time(self):
+        if hasattr(self, "times"):
+            return self.times.min()
+        return 0.0
+
+    def get_max_time(self):
+        return self.times.max()
+
+    def get_field_to_plot(
+        self,
+        time=None,
+        idx_time=None,
+        key=None,
+        equation=None,
+        interpolate_time=True,
+    ):
+
+        if time is None and idx_time is None:
+            self.update_times()
+            time = self.times[-1]
+
+        # Assert files are available
+        if self.times.size == 0:
+            raise FileNotFoundError(
+                f"No phys files were detected in directory: {self.path_dir}"
+            )
+
+        if not interpolate_time and time is not None:
+            idx, time_closest = self.get_closest_time_file(time)
+            return self.get_field_to_plot(
+                idx_time=idx, key=key, equation=equation
+            )
+
+        if interpolate_time and time is not None:
+
+            idx_closest, time_closest = self.get_closest_time_file(time)
+
+            if math.isclose(time, time_closest) or self.times.size == 1:
+                return self.get_field_to_plot(
+                    idx_time=idx_closest, key=key, equation=equation
+                )
+
+            if idx_closest == self.times.size - 1:
+                idx0 = idx_closest - 1
+                idx1 = idx_closest
+            elif time_closest < time or idx_closest == 0:
+                idx0 = idx_closest
+                idx1 = idx_closest + 1
+            elif time_closest > time:
+                idx0 = idx_closest - 1
+                idx1 = idx_closest
+
+            dt_save = self.times[idx1] - self.times[idx0]
+            weight0 = 1 - np.abs(time - self.times[idx0]) / dt_save
+            weight1 = 1 - np.abs(time - self.times[idx1]) / dt_save
+
+            field0, time0 = self.get_field_to_plot(
+                idx_time=idx0, key=key, equation=equation
+            )
+            field1, time1 = self.get_field_to_plot(
+                idx_time=idx1, key=key, equation=equation
+            )
+
+            return field0 * weight0 + field1 * weight1, time
+
+        return self._get_field_to_plot_from_file(
+            self.path_files[idx_time], key, equation
+        )
+
+    def get_closest_time_file(self, time):
+        """Find the index and value of the closest actual time of the field."""
+        idx = np.abs(self.times - time).argmin()
+        return idx, self.times[idx]
+
+    @abstractmethod
+    def _get_field_to_plot_from_file(self, path_file, key, equation):
         ...
