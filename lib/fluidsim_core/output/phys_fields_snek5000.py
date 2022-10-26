37e01e24d48c25a7db4b9c02316a8060fc5c362d
paugier <pierre.augier@univ-grenoble-alpes.fr>
2022-10-26 12:50:14 +0200

    New fluidsim_core/tests/test_output_movies.py and refactor fluidsim_core/tests/test_hexa_files.py

---
+++ lib/fluidsim_core/output/phys_fields_snek5000.py
@@ -0,0 +1,54 @@
+from functools import lru_cache
+
+from fluidsim_core.output.phys_fields import PhysFieldsABC
+from fluidsim_core.output.movies import MoviesBasePhysFieldsHexa
+from fluidsim_core.hexa_files import SetOfPhysFieldFiles
+
+
+class PhysFields4Snek5000(PhysFieldsABC):
+
+    _cls_movies = MoviesBasePhysFieldsHexa
+    _cls_set_of_files = SetOfPhysFieldFiles
+
+    def __init__(self, output=None):
+
+        self.set_of_phys_files = self._cls_set_of_files(output=output)
+        self.plot_hexa = self.set_of_phys_files.plot_hexa
+
+        self.movies = self._cls_movies(output, self)
+        self.animate = self.movies.animate
+        self.interact = self.movies.interact
+        self._equation = None
+
+    def get_key_field_to_plot(self, forbid_compute=False, key_prefered=None):
+        return self.set_of_phys_files.get_key_field_to_plot(key_prefered)
+
+    def get_field_to_plot(
+        self,
+        key=None,
+        time=None,
+        idx_time=None,
+        equation=None,
+        interpolate_time=True,
+    ):
+        """Get the field to be plotted in process 0."""
+        return self.set_of_phys_files.get_field_to_plot(
+            time=time,
+            idx_time=idx_time,
+            key=key,
+            equation=equation,
+            interpolate_time=interpolate_time,
+        )
+
+    def get_vector_for_plot(
+        self, from_state=False, time=None, interpolate_time=True
+    ):
+        if from_state:
+            raise ValueError("cannot get anything from the state for this solver")
+        return self.set_of_phys_files.get_vector_for_plot(time, self._equation)
+
+    @lru_cache(maxsize=None)
+    def _get_axis_data(self, equation=None):
+        hexa_x, _ = self.get_field_to_plot(idx_time=0, key="x", equation=equation)
+        hexa_y, _ = self.get_field_to_plot(idx_time=0, key="y", equation=equation)
+        return hexa_x, hexa_y
--- lib/fluidsim_core/tests/test_hexa_files.py
+++ lib/fluidsim_core/tests/test_hexa_files.py
@@ -1,9 +1,5 @@
 from dataclasses import dataclass
-import sys
 
-import pytest
-
-import numpy as np
 import matplotlib.pyplot as plt
 
 import pymech
@@ -18,73 +14,18 @@ class MockEvent:
     ydata: float
 
 
-name_solver = "cbox"
+def test_setoffiles(false_output):
 
-
-@pytest.fixture
-def path_sim(tmp_path):
-
-    path_dir = tmp_path / "session_00"
-    path_dir.mkdir(exist_ok=True, parents=True)
-
-    nx = ny = nz = 2
-    nx_elem = ny_elem = nz_elem = 2
-
-    hexa_data = pymech.core.HexaData(
-        ndim=3,
-        nel=nx_elem * ny_elem * nz_elem,
-        lr1=(nx, ny, nz),
-        var=(3, 3, 1, 1, 0),
-    )
-    hexa_data.wdsz = 8
-    hexa_data.istep = 0
-    hexa_data.endian = sys.byteorder
-
-    x1d = np.linspace(0, 1, nx)
-    y1d = np.linspace(0, 1, ny)
-    z1d = np.linspace(0, 1, nz)
-
-    y3d, z3d, x3d = np.meshgrid(x1d, y1d, z1d)
-
-    ielem = 0
-    for iz_elem in range(nz_elem):
-        for iy_elem in range(ny_elem):
-            for ix_elem in range(nx_elem):
-                elem = hexa_data.elem[ielem]
-                ielem += 1
-                elem.pos[0] = x3d + ix_elem
-                elem.pos[1] = y3d + iy_elem
-                elem.pos[2] = z3d + iz_elem
-
-    time = 2.0
-    for it in range(4):
-        hexa_data.time = time
-        pymech.writenek(path_dir / f"{name_solver}0.f{it:05d}", hexa_data)
-        time += 0.5
-
-    return path_dir.parent
-
-
-def test_setoffiles(path_sim):
+    output = false_output
 
     hexa_data_loaded = pymech.readnek(
-        path_sim / f"session_00/{name_solver}0.f{1:05d}"
+        output.path_run / f"session_00/{output.name_solver}0.f{1:05d}"
     )
 
     HexaField(hexa_data=hexa_data_loaded, key="vz")
     HexaField(hexa_data=hexa_data_loaded, key="pres", equation=None)
 
-    class Object:
-        pass
-
-    output = Object()
-    output.name_solver = "cbox"
-    output.sim = Object()
-    output.sim.params = Object()
-    output.sim.params.output = Object()
-    output.sim.params.output.session_id = 0
-
-    set_of_files = SetOfPhysFieldFiles(path_sim, output=output)
+    set_of_files = SetOfPhysFieldFiles(output.path_run, output=output)
     hexa_z, time = set_of_files.get_field_to_plot(2, key="z", equation=None)
 
     set_of_files.get_key_field_to_plot("pressure")
