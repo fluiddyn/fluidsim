7e13f5e130d80ce5bd915eaf1de71e530bc178bf
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-05-01 23:10:32 +0200

    fluidsim_core/output/dataframe_from_paths.py

--- fluidsim/util/util.py
+++ fluidsim/util/util.py
@@ -30,6 +30,7 @@ from fluiddyn.util import mpi
 from fluiddyn.util import get_memory_usage
 
 from fluidsim_core import loader
+from fluidsim_core.output.dataframe_from_paths import DataframeMaker
 
 from fluidsim import path_dir_results
 
@@ -795,127 +796,23 @@ def get_last_time_spatial_means_from_path(path):
         return json.loads(line_prev)["t"]
 
 
-def get_mean_values_from_path(
-    path, tmin=None, tmax=None, use_cache=True, customize=None
-):
+class _DataframeMakerFluidsim(DataframeMaker):
 
-    """Get a dict of scalar values characterizing the simulation
-
-    Parameters
-    ----------
-
-    tmin: float
-        Minimum time
-
-    tmax: float
-        Maximum time
-
-    use_cache: bool
-        If True, return the cached result
-
-    customize: callable
-
-        If not None, called as ``customize(result, self.sim)`` to modify the
-        returned dict.
-
-    Examples
-    --------
-
-    .. code-block:: python
-
-        def customize(result, sim):
-            result["Rb"] = float(sim.params.short_name_type_run.split("_Rb")[-1])
-        get_mean_values_from_path(path, customize=customize)
-
-    """
-
-    if (
-        tmin is None
-        or isinstance(tmin, str)
-        or tmax is None
-        or isinstance(tmax, str)
-    ):
+    def get_time_start_from_path(self, path):
+        """ "Get first time"""
         t_start, _ = times_start_last_from_path(path)
-        t_last = get_last_time_spatial_means_from_path(path)
+        return t_start
 
-    if tmin is None:
-        tmin = t_start
-    elif isinstance(tmin, str):
-        if tmin.startswith("t_start+"):
-            tmin = t_start + float(tmin.split("t_start+")[-1])
-        elif tmin.startswith("t_last-"):
-            tmin = t_last - float(tmin.split("t_last-")[-1])
-        else:
-            raise ValueError(
-                f"isinstance(tmin, str) and {tmin=} but tmin has to start by "
-                '"t_start+" or "t_last-"'
-            )
-    tmin = float(tmin)
+    def get_time_last_from_path(self, path):
+        """ "Get last saved time"""
+        return get_last_time_spatial_means_from_path(path)
 
-    if tmax is None:
-        tmax = t_last
-    elif isinstance(tmax, str):
-        if tmax.startswith("t_start+"):
-            tmax = t_start + float(tmax.split("t_start+")[-1])
-        elif tmax.startswith("t_last-"):
-            tmax = t_last - float(tmax.split("t_last-")[-1])
-        else:
-            raise ValueError(
-                f"isinstance(tmax, str) and {tmax=} but tmin has to start by "
-                '"t_start+" or "t_last-"'
-            )
-    tmax = float(tmax)
-
-    cache_dir = Path(path) / ".cache"
-    cache_dir.mkdir(exist_ok=True)
-
-    if customize is not None:
-        source = inspect.getsource(customize).encode().strip()
-        hash = hashlib.sha256(source).hexdigest()[:16]
-        part_customize = f"_customize{hash}"
-    else:
-        part_customize = ""
-
-    cache_file = cache_dir / (
-        f"mean_values_tmin{tmin}_tmax{tmax}{part_customize}.json"
-    )
-
-    if use_cache and cache_file.exists():
-        with open(cache_file, "r") as file:
-            return json.load(file)
-
-    sim = load_sim_for_plot(path, hide_stdout=True)
-
-    result = sim.output._compute_mean_values(tmin, tmax)
-    if customize is not None:
-        customize(result, sim)
-
-    print("saving", cache_file)
-    with open(cache_file, "w") as file:
-        json.dump(result, file, indent=2)
-    return result
+    def load_sim(self, path):
+        """Load a simulation object"""
+        return load_sim_for_plot(path, hide_stdout=True)
 
 
-def get_dataframe_from_paths(
-    paths, tmin=None, tmax=None, use_cache=True, customize=None
-):
-    """Produce a dataframe from a set of simulations.
+_dataframe_maker = _DataframeMakerFluidsim()
 
-    Uses ``sim.output.get_mean_values``
-
-    """
-
-    from pandas import DataFrame
-
-    values = []
-    for path in track(paths, "Getting the mean values"):
-        values.append(
-            get_mean_values_from_path(path, tmin, tmax, use_cache, customize)
-        )
-
-    df = DataFrame(values)
-
-    if "R2" in df.columns and "R4" in df.columns:
-        df["min_R"] = np.array([df.R2, df.R4]).min(axis=0)
-
-    return df
+get_mean_values_from_path = _dataframe_maker.get_mean_values_from_path
+get_dataframe_from_paths = _dataframe_maker.get_dataframe_from_paths
