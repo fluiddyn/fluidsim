487bd279a33ae12a524e37eb17577668c6183db4
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-06-07 14:35:13 +0200

    Small improvement start_ipython_load_sim

--- lib/fluidsim_core/ipy_load.py
+++ lib/fluidsim_core/ipy_load.py
@@ -4,14 +4,24 @@
 
 """
 
+import argparse
 from textwrap import dedent
 
 
 def start_ipython_load_sim(load_import="from fluidsim import load"):
     """Start IPython and load a simulation"""
+
+    parser = argparse.ArgumentParser(
+        prog="ipy-load",
+        description="Start IPython and load a simulation",
+    )
+    parser.add_argument("path_dir", nargs="?", default=None)
+    args = parser.parse_args()
+
     from IPython import start_ipython
 
     argv = ["--matplotlib", "-i", "-c"]
+
     code = dedent(
         f"""
         import numpy as np
@@ -24,5 +34,13 @@ def start_ipython_load_sim(load_import="from fluidsim import load"):
         print("`sim`, `params`, `np`, `plt` and `pd` variables are available")
     """
     )
-    argv.append("; ".join(code.strip().split("\n")))
+    lines = code.strip().split("\n")
+
+    if args.path_dir is not None:
+        from fluidsim_core.paths import find_path_result_dir
+
+        path_dir = find_path_result_dir(args.path_dir)
+        lines.insert(0, f"import os; os.chdir('{path_dir}')")
+
+    argv.append("; ".join(lines))
     start_ipython(argv=argv)
