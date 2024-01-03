c6189c2a238cf69ecf9bcc7e34e483a0c5605eb6
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-01-03 22:25:12 +0100

    Nox for test-coverage without fft and pythran

--- noxfile.py
+++ noxfile.py
@@ -8,3 +8,16 @@ os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 def validate_code(session):
     session.run_always("pdm", "install", "-G", "dev", "--no-self", external=True)
     session.run("pdm", "validate_code", external=True)
+
+
+@nox.session
+def test_without_fft_and_pythran(session):
+    command = "pdm install -G dev -G  test -G mpi -G scipy --no-self"
+    session.run_always(*command.split(), external=True)
+    session.install(".", "--config-settings=setup-args=-Dtransonic-backend=python")
+    session.run(
+        "make",
+        "_tests_coverage",
+        external=True,
+        env={"TRANSONIC_BACKEND": "python", "TRANSONIC_NO_REPLACE": "1"},
+    )
