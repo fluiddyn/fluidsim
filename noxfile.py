2cef935d44976a72d0878a720a513d3f3ac7d04a
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-01-04 11:35:07 +0100

    test_with_fft_and_pythran with nox

--- noxfile.py
+++ noxfile.py
@@ -12,7 +12,7 @@ def validate_code(session):
 
 @nox.session
 def test_without_fft_and_pythran(session):
-    command = "pdm install -G dev -G  test -G mpi -G scipy --no-self"
+    command = "pdm install -G dev -G test -G mpi -G scipy --no-self"
     session.run_always(*command.split(), external=True)
     session.install(".", "--config-settings=setup-args=-Dtransonic-backend=python")
     session.run(
@@ -21,3 +21,15 @@ def test_without_fft_and_pythran(session):
         external=True,
         env={"TRANSONIC_BACKEND": "python", "TRANSONIC_NO_REPLACE": "1"},
     )
+
+
+@nox.session
+def test_with_fft_and_pythran(session):
+    # first install fluidfft without Pythran compilation
+    session.install("fluidfft", env={"FLUIDFFT_TRANSONIC_BACKEND": "python"})
+
+    command = "pdm install -G dev -G test -G fft -G mpi -G scipy --no-self"
+    session.run_always(*command.split(), external=True)
+    session.install(".")
+
+    session.run("make", "_tests_coverage", external=True)
